bfc4cb64e9f6373abcd19cbbe5c58243adc0b54d
Nicholas Nelson <nelsonni@oregonstate.edu>
2017-08-05 18:13:51 -0700

    Card metadata fields, update mechanism, and test added

--- app/Card.js
+++ app/Card.js
@@ -3,8 +3,22 @@
 module.exports = class Card {
   constructor(id = throwIfMissing('id')) {
     this.id = id;
+    this.createdBy = require('username').sync();
+    this.createdTimestamp = new Date();
+    this.lastInteraction = new Date();
   }
 
+  updateMetadata() {
+    this.lastInteraction = new Date();
+  }
+
+  toggleDraggable() {
+
+  }
+
+  toggleDroppable() {
+
+  }
 }
 
 function throwIfMissing(param) {
