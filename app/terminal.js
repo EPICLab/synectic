3573673701eab370c4c9f29ffdbfdc0509a66046
Brandon Lee Dring <Brandon.Dring@cdk.com>
2017-09-14 20:15:57 -0700

    Much more elegant way to create a terminal card.

--- app/terminal.js
+++ app/terminal.js
@@ -1,52 +1,23 @@
 let Card = require("./Card")
 let xterm = require("xterm")
 let request = require("request")
-
+const webSock = require('ws');
 
 module.exports = class Terminal extends Card {
   constructor(type) {
     super(type);
     xterm.loadAddon('attach')
-    this.terminalWindow = new xterm()
-    // this.terminalWindow.attach = require("./../node_modules/xterm/dist/addons/attach/attach").attach
-
-    console.log(this.terminalWindow.attach)
-    this.terminalWindow.open(document.getElementById(this.body.id))
-    this.initTerminalListeners();
-    let f, ws;
-    request.post("http://localhost:6789/terminals", (e, r, body) => {
-      console.log(r)
+    xterm.loadAddon('fit')
+    this.terminalWindow = new xterm({
+      cursorBlink: true
+    })
+    this.ws = null;
+    request.post("http://localhost:6789/terminals?cols=20&rows=14", (err, resp, body) => {
       this.terminalWindow.open(document.getElementById(this.body.id))
-      const websock = require('ws');
-      ws = new websock('ws://localhost:6789/terminals/' + r.body);
-
-      ws.onopen = () => {
-        this.terminalWindow.attach(ws)
-      }
-    });
-    // console.log(new WebSocket())
-  }
-
-  initTerminalListeners() {
-    let self = this;
-    self.terminalWindow.prompt = function() {
-      self.terminalWindow.write('\r\n$');
-    };
-    this.terminalWindow.on('key', function(key, ev) {
-      var printable = (!ev.altKey && !ev.altGraphKey && !ev.ctrlKey && !ev.metaKey);
-
-      if (ev.keyCode == 13) {
-        self.terminalWindow.prompt();
-      } else if (ev.keyCode == 8) {
-        // Do not delete the prompt
-        if (self.terminalWindow.x > 2) {
-          self.terminalWindow.write('\b \b');
-        }
-      } else if (printable) {
-        console.log(key)
-        self.terminalWindow.write(key);
-      }
+      this.terminalWindow.fit()
+      this.terminalWindow.resize(20, 14) // resize to fit against the card
+      this.ws = new webSock('ws://localhost:6789/terminals/' + resp.body);
+      this.ws.onopen = () => this.terminalWindow.attach(this.ws)
     });
   }
-
 }
