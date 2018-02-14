5edfdf01f3254f70468869f8b716283e8417de12
Nicholas Nelson <nelsonni@oregonstate.edu>
2018-02-13 21:48:50 -0800

    Adding  package for browserify compatible Ace Editor, initial CodeEditor class

---
+++ src/app/CodeEditor.ts
@@ -0,0 +1,28 @@
+import { Card } from '../core/Card';
+import { Canvas } from '../core/Canvas';
+import { Stack } from '../core/Stack';
+import * as ace from 'brace';
+import 'brace/mode/javascript';
+import 'brace/theme/monokai';
+
+export class CodeEditor extends Card {
+
+  public editor: HTMLDivElement;
+
+  constructor(parent: Canvas | Stack) {
+    super(parent);
+
+    this.editor = document.createElement('div');
+    this.editor.setAttribute('id', (this.uuid + '-editor'));
+    this.addClass('editor');
+    $(this.editor).css({
+      width: '100%',
+      height: '100%'
+    });
+    this.element.appendChild(this.editor);
+
+    const editor = ace.edit(this.uuid + '-editor');
+    editor.getSession().setMode('ace/mode/javascript');
+    editor.setTheme('ace/theme/monokai');
+  }
+}
--- src/core/renderer.ts
+++ src/core/renderer.ts
@@ -1,5 +1,6 @@
 import { AppManagerInstance } from './AppManager';
 import { Card } from './Card';
+import { CodeEditor } from '../app/CodeEditor';
 import '../asset/style/canvas.css';
 import '../asset/style/card.css';
 import '../asset/style/stack.css';
@@ -12,3 +13,8 @@ const newCardButton = document.createElement('button');
 newCardButton.innerText = 'New Card';
 newCardButton.onclick = () => new Card(c);
 c.element.appendChild(newCardButton);
+
+const newEditorButton = document.createElement('button');
+newEditorButton.innerText = 'New Editor';
+newEditorButton.onclick = () => new CodeEditor(c);
+c.element.appendChild(newEditorButton);
