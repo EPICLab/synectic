a16b955886ed95ab52ba9043f757642c5a462e10
Nicholas Nelson <nelsonni@oregonstate.edu>
2018-02-25 22:24:06 -0800

    Linking file selection dialog to asynchronous file input for TextCard, plus filetype filters

--- src/app/TextCard.ts
+++ src/app/TextCard.ts
@@ -1,40 +1,22 @@
 import { Card } from '../core/Card';
 import { Canvas } from '../core/Canvas';
 import { Stack } from '../core/Stack';
-import { testLoad, asyncLoad } from '../core/System';
+import { openDialog } from '../core/System';
 import { addClass } from '../core/helper';
-import { remote } from 'electron';
 
 export class TextCard extends Card {
 
   public loadButton: HTMLButtonElement = document.createElement('button');
-  public loadASyncButton: HTMLButtonElement = document.createElement('button');
-  public selectorButton: HTMLButtonElement = document.createElement('button');
   public content: HTMLDivElement = document.createElement('div');
 
   constructor(parent: Canvas | Stack) {
     super(parent);
     addClass(this.content, 'card-content');
 
-    this.loadButton.innerText = 'Load Textfile';
-    this.loadButton.onclick = () => console.log(testLoad('/foo.txt'));
-
-    this.loadASyncButton.innerText = 'Load ASync';
-    this.loadASyncButton.onclick = () => asyncLoad('/foo.txt', this.content);
-
-    this.selectorButton.innerText = 'Select File';
-    this.selectorButton.onclick = () => {
-      remote.dialog.showOpenDialog(remote.getCurrentWindow(), {}, function(fileNames) {
-        if (fileNames === undefined) return;
-        fileNames.map((filename) => {
-          console.log(filename);
-        });
-      });
-    };
+    this.loadButton.innerText = 'Select File';
+    this.loadButton.onclick = () => openDialog({ filters: [{name: 'text', extensions:['txt']}] }, this.content);
 
     this.element.appendChild(this.loadButton);
-    this.element.appendChild(this.loadASyncButton);
-    this.element.appendChild(this.selectorButton);
     this.element.appendChild(this.content);
   }
 }
--- src/core/System.ts
+++ src/core/System.ts
@@ -1,5 +1,6 @@
 import * as url from 'url';
 import * as fs from 'fs-extra';
+import { OpenDialogOptions, remote } from 'electron';
 
 export function testLoad(path: string): string {
   let fullPath: string = __dirname + path;
@@ -11,9 +12,19 @@ export function testLoad(path: string): string {
   return fs.readFileSync(fullPath, 'utf8');
 }
 
+export function openDialog(options: OpenDialogOptions, target: HTMLDivElement) {
+  remote.dialog.showOpenDialog(remote.getCurrentWindow(), options,
+    (filenames: string[]) => {
+      if (filenames === undefined) return;
+      return asyncLoad(filenames[0], target);
+    }
+  );
+}
+
 export function asyncLoad(path: string, target: HTMLDivElement): void {
-  let fullPath: string = __dirname + path;
-  fs.readFile(fullPath).then((res: Buffer) => {
-    target.innerText = res.toString();
-  });
+  fs.readFile(path)
+    .then((res: Buffer) => {
+      target.innerText = res.toString();
+    })
+    .catch(err => console.log(err));
 }
