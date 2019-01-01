ae17df98f9665b9e679b9669338b9176797a7185
Nicholas Nelson <nelsonni@oregonstate.edu>
2018-12-31 23:33:03 -0800

    Asynchronous and parallel processing of file content, filetypes, and Ace editor mode setting for Editor per #51 and #38

--- src/app/editor/Editor.ts
+++ src/app/editor/Editor.ts
@@ -8,6 +8,9 @@ import 'brace/mode/typescript';
 import 'brace/mode/latex';
 import 'brace/mode/python';
 import 'brace/theme/monokai';
+import { extname, readFileAsync } from '../../core/fs/io';
+import { searchExt } from '../../core/fs/filetypes';
+import { snackbar } from '../../core/fs/notifications';
 
 export class Editor extends Card {
 
@@ -32,7 +35,37 @@ export class Editor extends Card {
     this.element.appendChild(this.editorWindow);
 
     this.editor = ace.edit(this.uuid + '-editor');
-    this.editor.getSession().setMode('ace/mode/javascript');
     this.editor.setTheme('ace/theme/monokai');
+    if (filename !== '') this.loadContent(this.filename);
+  }
+
+  /**
+   * Reads local file content into this Editor card.
+   * @param filename A valid filename or path for reading content into this card.
+   */
+  loadContent(filename: string): void {
+    Promise.all([readFileAsync(filename), searchExt(extname(filename))])
+    .then(result => {
+      let [content, filetype] = result;
+      this.setContent(content);
+      if (filetype !== undefined) this.setMode(filetype.name);
+    })
+    .catch(error => snackbar(global.Synectic.current, error.message, 'Editor Card Error: File Loading Failed'));
+  }
+
+  /**
+   * Sets the Ace editor to display content.
+   * @param content A string of content to be displayed in this card.
+   */
+  setContent(content: string): void {
+    this.editor.setSession(new ace.EditSession(content));
+  }
+
+  /**
+   * Sets the Ace editor mode for syntax highlighting and auto-completion.
+   * @param mode The name of an Ace editor mode (e.g. JavaScript).
+   */
+  setMode(mode: string): void {
+    this.editor.getSession().setMode('ace/mode/' + mode.toLowerCase());
   }
 }
