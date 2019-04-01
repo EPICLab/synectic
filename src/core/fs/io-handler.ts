e77c4a9289550382d64292bc6c1222b48f4cccfa
Nicholas Nelson <nelsonni@oregonstate.edu>
2019-04-01 14:47:19 -0700

    Updated handlerToCard function to create new file when file does not exist

--- src/core/fs/io-handler.ts
+++ src/core/fs/io-handler.ts
@@ -1,21 +1,31 @@
 import { Card } from '../lib/Card';
 import { cardTypes } from '../../app/Types';
+import * as io from '../fs/io';
 import { Dialog } from '../lib/Dialog';
 
 /**
  * Converts card type name to card instance on canvas.
  * @param handler The Card type name (use {@link app/Types.ts} for handler names).
  * @param filename A valid filename or path to associate with the new card.
- * @return Card instance corresponding to handler type or null if no handler defined.
+ * @return Card instance corresponding to handler type or undefined if no handler defined.
  */
-export function handlerToCard<T extends Card>(handler: string, filename?: string): T | null {
+export function handlerToCard<T extends Card>(handler: string, filename: string): T | undefined {
+  console.log(`handlerToCard => handler: ${handler}, filename: ${filename}`);
   if (handler in cardTypes) {
-    return new cardTypes[handler](global.Synectic.current, filename);
-  } else {
-    let message = 'Type handler `' + handler + '` is not a configured card type.';
-    message = message.concat(' Verify card type exists in app/Types.ts file and');
-    message = message.concat(' try reloading a file.');
-    new Dialog('banner', 'Typer Handler Configuration Error', message);
-    return null;
+    console.log(`handler (${handler}) found in cardTypes`);
+    io.exists(filename).then(exist => {
+      if (exist) {
+        console.log(`io.exists returned true for: '${filename}'`);
+        return new cardTypes[handler](global.Synectic.current, filename);
+      } else {
+        console.log(`io.exists returned false for: '${filename}'`);
+        io.writeFileAsync(filename, '')
+          .then(() => {
+            return new cardTypes[handler](global.Synectic.current, filename);
+          })
+          .catch(error => new Dialog('snackbar', 'New Card Loading Error', error.message));
+      }
+    });
   }
+  return undefined;
 }
