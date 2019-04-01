e7cc5c93163b972f7f76e069c3e4fc64bb79d0a7
Nicholas Nelson <nelsonni@oregonstate.edu>
2019-04-01 14:45:57 -0700

    Added exists function to core/fs/io.ts

--- src/core/fs/io.ts
+++ src/core/fs/io.ts
@@ -5,12 +5,12 @@ import * as path from 'path';
  * Return the extension of the path, after the last '.' to end of string in the last portion of the path.
  * If there is no '.' in the last portion of the path or the first character of it is '.',
  * then it returns the entire string.
- * @param p The path to evaluate.
+ * @param filepath The path to evaluate.
  */
-export function extname(p: string): string {
-  const ext: string | undefined = p.split('.').pop();
+export function extname(filepath: string): string {
+  const ext: string | undefined = filepath.split('.').pop();
   if (ext !== undefined) return ext;
-  else return p;
+  else return filepath;
 }
 
 /**
@@ -23,13 +23,31 @@ export function deserialize<T>(json: string): T {
 }
 
 /**
+ * Asynchronously checks for the existence of a file or directory within the
+ * local filesystem.
+ * @param filepath A valid filename or path to check.
+ * @return Boolean indicating file or directory exists within filesystem.
+ */
+export function exists(filepath: string): Promise<boolean> {
+  return new Promise((resolve, _) => {
+    fs.stat(filepath)
+      .then(() => {
+        resolve(true);
+      })
+      .catch(() => {
+        resolve(false);
+      });
+  });
+}
+
+/**
  * Asynchronously reads file content into a string.
- * @param filename A valid filename or path to read from.
+ * @param filepath A valid filename or path to read from.
  * @return A string containing the file content.
  */
-export function readFileAsync(filename: string): Promise<string> {
+export function readFileAsync(filepath: string): Promise<string> {
   return new Promise((resolve, reject) => {
-    fs.readFile(path.resolve(filename), (error, result) => {
+    fs.readFile(path.resolve(filepath), (error, result) => {
       if (error) reject(error);
       else resolve(result.toString());
     });
@@ -38,16 +56,16 @@ export function readFileAsync(filename: string): Promise<string> {
 
 /**
  * Asynchronously writes to a file; creates a new file if none exists.
- * @param filename A valid filename or path to write the data to.
+ * @param filepath A valid filename or path to write the data to.
  * @param data A string containing content.
  */
-export function writeFileAsync(filename: string, data: string): Promise<void> {
+export function writeFileAsync(filepath: string, data: string): Promise<void> {
   return new Promise((resolve, reject) => {
-    fs.writeFile(path.resolve(filename), data, (error) => {
+    fs.writeFile(path.resolve(filepath), data, (error) => {
       if (error) {
         reject(error);
       } else {
-        console.info('File `' + path.resolve(filename) + '` created.');
+        console.info('File `' + path.resolve(filepath) + '` created.');
         resolve();
       }
     });
