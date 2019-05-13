4b62998d6f7c376b2557e765927f4a9d4e7cf57e
Nicholas Nelson <nelsonni@oregonstate.edu>
2019-05-13 16:02:48 -0700

    Git Infra 1 of 3: GitManager service for managing and responding to requests for Repository instances related to files within .git managed project directories

--- src/core/lib/AppManager.ts
+++ src/core/lib/AppManager.ts
@@ -1,21 +1,28 @@
 import { Canvas } from './Canvas';
 import { EventDispatcher } from '../events/events';
 import { CredentialManager } from '../vcs/CredentialManager';
+import { GitManager } from '../vcs/GitManager';
 import * as git from 'isomorphic-git';
+import { PathLike } from 'fs-extra';
+import * as path from 'path';
 import fs from 'fs';
 git.plugins.set('fs', fs);
+import os from 'os';
 
 class AppManager {
 
   private static _instance: AppManager;
   public canvasList: Canvas[];
+  public GitManager: GitManager;
   public credentialManager: CredentialManager;
   public events: EventDispatcher;
+  public appdir: PathLike = path.join(os.homedir(), '/.synectic/');
 
   private constructor() {
     this.canvasList = new Array();
     this.events = new EventDispatcher();
     this.credentialManager = new CredentialManager();
+    this.GitManager = new GitManager();
     git.plugins.set('credentialManager', this.credentialManager);
   }
 
@@ -31,7 +38,6 @@ class AppManager {
     this.canvasList.push(new Canvas([]));
     return this.current;
   }
-
 }
 
 export const AppManagerInstance = AppManager.Instance;
