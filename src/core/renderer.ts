954aa8c9dde6ac09cf6994f70521c1bdcd4fe3c9
Nicholas Nelson <nelsonni@oregonstate.edu>
2019-05-13 15:44:08 -0700

    Dialog test buttons removed from Canvas, new git-related button icons added

--- src/core/renderer.ts
+++ src/core/renderer.ts
@@ -7,7 +7,6 @@ import '../asset/style/notification.css';
 
 import { openCardDialog, newCardDialog } from './fs/dialogs';
 import * as git from './vcs/git';
-import { Dialog } from './lib/Dialog';
 
 global.Synectic = AppManagerInstance;
 const c = global.Synectic.newCanvas();
@@ -21,27 +20,6 @@ const loadCardButton = document.createElement('button');
 loadCardButton.innerText = 'Open...';
 loadCardButton.onclick = () => openCardDialog({});
 c.element.appendChild(loadCardButton);
-
-const snackbarButton = document.createElement('button');
-snackbarButton.innerText = 'Snackbar';
-snackbarButton.onclick = () => {
-  new Dialog('snackbar', 'Snackbar Test', 'Testing...');
-};
-
-const bannerButton = document.createElement('button');
-bannerButton.innerText = 'Banner';
-bannerButton.onclick = () => {
-  new Dialog('banner', 'Banner Test', 'Testing...');
-};
-
-const dialogButton = document.createElement('button');
-dialogButton.innerText = 'Dialog';
-dialogButton.onclick = () => {
-  new Dialog('dialog', 'Dialog Test', 'Testing...');
-};
-c.element.appendChild(snackbarButton);
-c.element.appendChild(bannerButton);
-c.element.appendChild(dialogButton);
 
 const testCredentials = document.createElement('button');
 testCredentials.innerText = 'Test Credentials...';
