9005c4a98bf3a1a35fb0c7ba2e1247afe98f6dcb
Nicholas Nelson <nelsonni@oregonstate.edu>
2019-05-13 15:59:39 -0700

    Method docs updated for helper functions

--- src/core/lib/helper.ts
+++ src/core/lib/helper.ts
@@ -11,7 +11,8 @@ export function hasClass(el: HTMLElement, className: string): boolean {
 /**
  * Helper function to add a CSS class to an HTML element.
  * @param el Target HTML element to add CSS class.
- * @param className CSS class name to be added to target; there is no effect if CSS class is already present in the CSS class list of target.
+ * @param className CSS class name to be added to target; there is no effect if
+ * CSS class is already present in the CSS class list of target.
  */
 export function addClass(el: HTMLElement, className: string): void {
   if (el.classList) el.classList.add(className);
@@ -21,12 +22,22 @@ export function addClass(el: HTMLElement, className: string): void {
 /**
  * Helper function to remove CSS class from an HTML element.
  * @param el Target HTML element to remove CSS class.
- * @param className CSS class to be removed from target; there is no effect if CSS class is not present in the CSS class list of target.
+ * @param className CSS class to be removed from target; there is no effect if CSS
+ * class is not present in the CSS class list of target.
  */
 export function removeClass(el: HTMLElement, className: string): void {
   const exp: RegExp = new RegExp('\\b' + className + '\\b', 'g');
   if (el.classList) el.classList.remove(className);
   else el.className = el.className.replace(exp, '');
+}
+
+/**
+ * Helper function to toggle CSS class from an HTML element; adding if CSS class
+ * is not already added, or removing CSS class if previously added.
+ */
+export function toggleClass(el: HTMLElement, className: string): void {
+  if (hasClass(el, className)) removeClass(el, className);
+  else addClass(el, className);
 }
 
 /**
@@ -48,17 +59,18 @@ export function applyMixins(derivedCtor: any, baseCtors: any[]): void {
 /**
  * Toggle the show/hide state visibility state of an HTMLElement contained within this card.
  * @param elem HTMLElement or deriving child element types.
- * @param visiblity Optional setting for explicitly setting show/hide state; true shows the element, false hides the element.
+ * @param visiblity Optional setting for explicitly setting show/hide state;
+ * true shows the element, false hides the element.
  */
 export function toggleVisibility(elem: HTMLElement, visiblity?: boolean): void {
   switch (visiblity) {
-    case true:
-      $(elem).show();
-      break;
-    case false:
-      $(elem).hide();
-      break;
-    default:
-      $(elem).toggle();
+  case true:
+    $(elem).show();
+    break;
+  case false:
+    $(elem).hide();
+    break;
+  default:
+    $(elem).toggle();
   }
 }
