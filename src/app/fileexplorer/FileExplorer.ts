71b9c7972d247002868eea802f2657a626ff6f6d
Ben Klein <robobenklein@gmail.com>
2019-05-15 21:12:56 -0400

    Gah it looks ugly but it works

--- src/app/fileexplorer/FileExplorer.ts
+++ src/app/fileexplorer/FileExplorer.ts
@@ -32,38 +32,48 @@ function safeReadDirSync(path: fs.PathLike): string[] | null {
   return dirData;
 }
 
-const enum ExplorerLazyLoadItemMode {
+const enum FileExplorerLazyPathItemMode {
   active,
   lazy
 }
 /**
- * a ExplorerLazyLoadItem for representing stuff only when needed.
+ * a FileExplorerLazyPathItem for representing stuff only when needed.
  *
  * active -> will rescan and watch children
  * lazy -> does not rescan unless asked to or becomes active
  */
-class ExplorerLazyLoadItem {
+class FileExplorerLazyPathItem {
   path: fs.PathLike;
   name: string;
-  state: ExplorerLazyLoadItemMode;
-  children: Map<string, ExplorerLazyLoadItem> = new Map<string, ExplorerLazyLoadItem>();
-  type: filetype | undefined;
+  state: FileExplorerLazyPathItemMode;
+  children: Map<string, FileExplorerLazyPathItem> = new Map<string, FileExplorerLazyPathItem>();
+  type: filetype;
   stats: fs.Stats;
 
   constructor(
     path: fs.PathLike,
     name: string,
-    state: ExplorerLazyLoadItemMode
+    state: FileExplorerLazyPathItemMode
   ) {
     this.path = path;
     this.name = name;
     this.state = state;
     this.stats = fs.statSync(path);
+    this.stats.isDirectory()? this.type = filetype.directory : this.type = filetype.file;
     this.update().then((result) => {
-      console.debug("ExplorerLazyLoadItem constructor update done:", result);
+      console.debug("FileExplorerLazyPathItem constructor update done:", result);
     }).catch((err) => {
-      console.error("ExplorerLazyLoadItem:", err);
+      console.error("FileExplorerLazyPathItem:", err);
     });
+  }
+
+  toggle_active_state() {
+    if (this.state == FileExplorerLazyPathItemMode.active) {
+      this.state = FileExplorerLazyPathItemMode.lazy;
+    }
+    else {
+      this.state = FileExplorerLazyPathItemMode.active;
+    }
   }
 
   /**
@@ -79,7 +89,7 @@ class ExplorerLazyLoadItem {
         reject(err);
       }
       if (
-        this.state === ExplorerLazyLoadItemMode.active &&
+        this.state === FileExplorerLazyPathItemMode.active &&
         this.stats.isDirectory()
       ) {
         // rescan for new children
@@ -92,10 +102,10 @@ class ExplorerLazyLoadItem {
           var child_promises = dirItems.map((dirItem) => {
             var child = this.children.get(dirItem);
             if (child == undefined) {
-              var newchild = new ExplorerLazyLoadItem(
+              var newchild = new FileExplorerLazyPathItem(
                 PATH.join(this.path.toString(), dirItem),
                 dirItem,
-                ExplorerLazyLoadItemMode.lazy
+                FileExplorerLazyPathItemMode.lazy
               );
               this.children.set(
                 dirItem,
@@ -126,36 +136,117 @@ class ExplorerLazyLoadItem {
   }
 }
 
+class FileExplorerDirView extends HTMLOListElement {
+  dirItem: FileExplorerLazyPathItem | undefined;
+  fe_children: Map<FileExplorerLazyPathItem, FileExplorerDirView | HTMLElement> = new Map<FileExplorerLazyPathItem, FileExplorerDirView | HTMLElement>();
+  fe_dropdown_name: HTMLElement;
+
+  constructor() {
+    super();
+    this.classList.add("fileexplorer-dir-view");
+    this.fe_dropdown_name = document.createElement('div');
+    this.fe_dropdown_name.classList.add("fileexplorer-dir-header");
+    this.fe_dropdown_name.innerText = "...";
+    this.appendChild(this.fe_dropdown_name);
+  }
+
+  setModel(dirItem: FileExplorerLazyPathItem) {
+    this.dirItem = dirItem;
+    this.fe_dropdown_name.innerText = this.dirItem.name;
+
+    // when we get clicked, it's active time!
+    this.fe_dropdown_name.onclick = (event) => {
+      console.log(this, event);
+      dirItem.toggle_active_state();
+      dirItem.update().then(() => {
+        this.update();
+      });
+      event.stopPropagation();
+    }
+  }
+
+  /**
+   * rebuild the visuals!
+   */
+  update(): void {
+    if (this.dirItem == undefined) return;
+    // @ts-ignore
+    this.dirItem.children.forEach((item, name, og_map) => {
+      // first checking for items which we don't yet have rendered:
+      var visual_child = this.fe_children.get(item);
+      if (visual_child == undefined) {
+        if (item.type == filetype.directory) {
+          visual_child = document.createElement('ol', {is: 'synectic-file-explorer-directory'});
+          (visual_child as FileExplorerDirView).setModel(item);
+          (visual_child as FileExplorerDirView).update();
+          this.fe_children.set(item, visual_child);
+          this.appendChild(visual_child);
+        }
+        else {
+          // it's a normal file
+          visual_child = document.createElement('li');
+          visual_child.classList.add('fileexplorer-file-item');
+          visual_child.innerText = item.name;
+          this.appendChild(visual_child);
+        }
+      }
+      else {
+        if ((visual_child as FileExplorerDirView).update) {
+          (visual_child as FileExplorerDirView).update();
+        }
+      }
+    });
+    if (this.dirItem.state === FileExplorerLazyPathItemMode.active) {
+      this.classList.add("expanded");
+      this.classList.remove("collapsed");
+    }
+    else {
+      this.classList.remove("expanded");
+      this.classList.add("collapsed");
+    }
+  }
+}
+
+window.customElements.define(
+  "synectic-file-explorer-directory",
+  FileExplorerDirView,
+  {
+    extends: 'ol'
+  }
+);
+
 export class FileExplorer extends Card {
 
   primaryContainerElement: HTMLDivElement = document.createElement('div');
-  mainItem: ExplorerLazyLoadItem;
+  mainItem: FileExplorerLazyPathItem;
+  mainView: HTMLOListElement;
 
   /**
    * Default constructor for creating a FileExplorer card.
    * @param parent A canvas or stack instance that will contain the new FileExplorer card.
    * @param directory A valid directory path to associate content with this FileExplorer card.
-   * @param gitrevision A commit ID that if specified will browse files at that commit,
-   *                    causes editor cards spawned to reference read-only file from commit
    */
   constructor(parent: Canvas | Stack, directory: fs.PathLike | null) {
     super(parent, directory? directory.toString() : process.cwd() );
     if (directory === null || directory === undefined) {
       directory = fs.realpathSync(process.cwd());
     }
-    this.mainItem = new ExplorerLazyLoadItem(
+    this.mainItem = new FileExplorerLazyPathItem(
       directory,
       PATH.basename(directory.toString()),
-      ExplorerLazyLoadItemMode.active
+      FileExplorerLazyPathItemMode.active
     );
+    this.mainView = document.createElement('ol', {is: 'synectic-file-explorer-directory'});
+    (this.mainView as FileExplorerDirView).setModel(this.mainItem);
 
     this.element.classList.add('fileexplorer');
     this.primaryContainerElement.setAttribute('id', (this.uuid + '-fileexplorer'));
     this.primaryContainerElement.setAttribute('class', 'fileexplorer-window');
     this.front.appendChild(this.primaryContainerElement);
 
-    this.load();
+    this.primaryContainerElement.appendChild(this.mainView);
 
+    this.load();
   }
 
   /**
@@ -164,7 +255,7 @@ export class FileExplorer extends Card {
   load(): void {
     console.log("Updating FileExplorer...");
     this.update().then(() => {
-      console.log("Now:", this.mainItem);
+      console.log("Loaded:", this);
     });
   }
 
@@ -175,13 +266,27 @@ export class FileExplorer extends Card {
   async update(): Promise<any> {
     return new Promise((resolve, reject) => {
       this.mainItem.update().then(() => {
-        // TODO update the visual elements now
-        //
-        resolve();
+        try {
+          (this.mainView as FileExplorerDirView).update();
+          resolve();
+        }
+        catch (err) {
+          console.error("Hmmm:", this);
+          throw err;
+        }
       }).catch((err: any) => {
         reject(err);
       });
     });
+  }
+
+  /**
+   * synchronous rebuild of the visual elements
+   *
+   * no I/O should be done here
+   */
+  refresh_view(): void {
+
   }
 
   /**
