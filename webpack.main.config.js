f382afc2e842599971f937da5941745c890f844b
Nicholas Nelson <nelsonni@oregonstate.edu>
2019-10-11 13:50:29 -0700

    TypeScript and ts-loader for webpack

--- webpack.main.config.js
+++ webpack.main.config.js
@@ -3,7 +3,10 @@ module.exports = {
    * This is the main entry point for your application, it's the first file
    * that runs in the main process.
    */
-  entry: './src/main.js',
+  entry: './src/main.ts',
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']  
+  },
   // Put your normal webpack config below here
   module: {
     rules: require('./webpack.rules'),
--- webpack.rules.js
+++ webpack.rules.js
@@ -14,20 +14,14 @@ module.exports = [
       },
     },
   },
-  // Put your webpack loader rules in this array.  This is where you would put
-  // your ts-loader configuration for instance:
-  /**
-   * Typescript Example:
-   *
-   * {
-   *   test: /\.tsx?$/,
-   *   exclude: /(node_modules|.webpack)/,
-   *   loaders: [{
-   *     loader: 'ts-loader',
-   *     options: {
-   *       transpileOnly: true
-   *     }
-   *   }]
-   * }
-   */
+  {
+    test: /\.tsx?$/,
+    exclude: /(node_modules|.webpack)/,
+    loaders: [{
+      loader: 'ts-loader',
+      options: {
+        transpileOnly: true
+      }
+    }]
+  }
 ];
