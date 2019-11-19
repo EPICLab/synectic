0b5d3095c362e10199c754f028fd2da1a9938d4e
Nicholas Nelson <nelsonni@oregonstate.edu>
2019-11-18 16:27:12 -0800

    minor tweaks to add parts of Redux store functionality

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import { DndProvider } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
+
+import { DateTime } from 'luxon';
+
 import Example from './old-components/Example';
 import Canvas from './components/Canvas';
 import UserList from './old-components/UserList';
-import { DateTime } from 'luxon';
 import './assets/style.css';
+import todos from './reducers/todos';
+
+const store = createStore(todos);
 
 const App = (): JSX.Element => {
   const users = [
@@ -15,14 +24,16 @@ const App = (): JSX.Element => {
   ]
 
   return (
-    <DndProvider backend={HTML5Backend}>
-      <React.Fragment>
-        <Canvas>
-          <UserList users={users}></UserList>
-          <Example />
-        </Canvas>
-      </React.Fragment>
-    </DndProvider>
+    <Provider store={store}>
+      <DndProvider backend={HTML5Backend}>
+        <React.Fragment>
+          <Canvas>
+            <UserList users={users}></UserList>
+            <Example />
+          </Canvas>
+        </React.Fragment>
+      </DndProvider>
+    </Provider>
   )
 };
 
