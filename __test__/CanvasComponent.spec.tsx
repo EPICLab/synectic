9afb053ee77ee290e64b7f997b3dd582de3354a9
Nicholas Nelson <nelsonni@oregonstate.edu>
2020-03-05 15:22:57 -0800

    Simplified CanvasComponent unit tests

--- __test__/CanvasComponent.spec.tsx
+++ __test__/CanvasComponent.spec.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-// import isUUID from 'validator/lib/isUUID';
+import isUUID from 'validator/lib/isUUID';
 import { mount } from 'enzyme';
 
 import { wrapInTestContext } from './__mocks__/dndReduxMock';
 import CanvasComponent from '../src/components/CanvasComponent';
-import { getMockStore, getCanvasProps } from './__mocks__/baseReduxMock';
+import { getMockStore, getCanvasProps } from './__mocks__/reduxStoreMock';
 import CardComponent from '../src/components/CardComponent';
+import StackComponent from '../src/components/StackComponent';
 
 describe('CanvasComponent', () => {
 
@@ -16,37 +17,24 @@ describe('CanvasComponent', () => {
   const store = getMockStore();
   const canvasProps = getCanvasProps();
 
-  it('Canvas accepts initial state of cards via props', () => {
+  it('Canvas resolves props into React Components for cards', () => {
     const CanvasContext = wrapInTestContext(CanvasComponent, store);
     const wrapper = mount(<CanvasContext {...canvasProps} />, mountOptions);
     const component = wrapper.find(CanvasComponent).first();
-    expect(component.props().cards).toHaveLength(2);
+    expect(wrapper.find(CardComponent)).toHaveLength(component.props().cards.length);
   });
 
-  it('Canvas accepts initial state of stacks via props', () => {
+  it('Canvas resolves props into React Components for stacks', () => {
     const CanvasContext = wrapInTestContext(CanvasComponent, store);
     const wrapper = mount(<CanvasContext {...canvasProps} />, mountOptions);
     const component = wrapper.find(CanvasComponent).first();
-    expect(component.props().stacks).toHaveLength(1);
+    expect(wrapper.find(StackComponent)).toHaveLength(component.props().stacks.length);
   });
 
-  it('Canvas accepts initial state of repos via props', () => {
+  it('Canvas has a valid UUID when props contain valid UUID', () => {
     const CanvasContext = wrapInTestContext(CanvasComponent, store);
     const wrapper = mount(<CanvasContext {...canvasProps} />, mountOptions);
     const component = wrapper.find(CanvasComponent).first();
-    expect(component.props().repos).toHaveLength(1);
+    expect(isUUID(component.props().id, 4)).toBe(true);
   });
-
-  it('Canvas contains dependent React Components for cards', () => {
-    const CanvasContext = wrapInTestContext(CanvasComponent, store);
-    const wrapper = mount(<CanvasContext {...canvasProps} />, mountOptions);
-    expect(wrapper.find(CardComponent)).toHaveLength(2);
-  });
-
-  // it('CanvasComponent has a valid UUID when props contain valid UUID', () => {
-  //   expect(enzymeWrapper).toBeDefined();
-  //   // const canvas = enzymeWrapper.find(CanvasComponent).first();
-  //   // const uuid = canvas.props().id ? canvas.props().id : '';
-  //   // expect(isUUID((uuid ? uuid : ''), 4)).toBe(true);
-  // });
 });
