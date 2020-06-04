7ca105087d1dcd53d56d855f155d553d04d53fbe
Nicholas Nelson <nelsonni@oregonstate.edu>
2020-06-03 17:38:35 -0700

    Fixed reload, removed Go button on Browser

--- src/components/Browser.tsx
+++ src/components/Browser.tsx
@@ -5,7 +5,7 @@ import { useDispatch } from 'react-redux';
 import { Button } from '@material-ui/core';
 import { Card } from '../types';
 import { Actions, ActionKeys } from '../store/actions';
-import { WebviewTag } from 'electron';
+// import { WebviewTag } from 'electron';
 import KeyboardArrowLeftIcon from '@material-ui/icons/KeyboardArrowLeft';
 import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
 import ReplayIcon from '@material-ui/icons/Replay';
@@ -20,9 +20,10 @@ type BrowserState = {
 export const BrowserComponent: React.FunctionComponent = () => {
   // this is related to the URL bar
   const [urlInput, setUrlInput] = useState('');
-  const [browserState, setBrowserState] = useState<BrowserState>({ history: [], current: new URL(''), index: 0 });
+  const [browserState, setBrowserState] = useState<BrowserState>({ history: [], current: new URL('https://epiclab.github.io/'), index: 0 })
 
-  const go = () => {
+  const go = (e: React.KeyboardEvent) => {
+    if (e.keyCode != 13) return;
     let history = browserState.history;
     if (browserState.index > 0) {
       history = history.slice(browserState.index);
@@ -46,20 +47,18 @@ export const BrowserComponent: React.FunctionComponent = () => {
   }
 
   const reloadSite = () => {
-    const webview = document.querySelector('webview') as WebviewTag;
-    webview.reload();
-
+    setBrowserState({ ...browserState, current: browserState.current });
   }
 
   return (
     <>
-      <KeyboardArrowLeftIcon onClick={() => backwards()} fontSize="default" color="primary" />
-      <KeyboardArrowRightIcon onClick={() => forwards()} fontSize="default" color="primary" />
-      <ReplayIcon onClick={reloadSite} fontSize="small" color="primary" />
-      <input type="text" placeholder="URL" value={urlInput} onChange={e => setUrlInput(e.target.value)} />
-      <button onClick={() => go()}>Go</button>
-
-      <div>
+      <div className='browser-topbar'>
+        <KeyboardArrowLeftIcon onClick={() => backwards()} fontSize="default" color="primary" />
+        <KeyboardArrowRightIcon onClick={() => forwards()} fontSize="default" color="primary" />
+        <ReplayIcon onClick={reloadSite} fontSize="small" color="primary" />
+        <input type="text" placeholder="URL" value={urlInput} onKeyDown={go} onChange={e => setUrlInput(e.target.value)} />
+      </div>
+      <div className='browser-content'>
         <webview src={browserState.current.toString()} style={{ height: '205px', width: '200px' }} ></webview>
       </div>
     </>
