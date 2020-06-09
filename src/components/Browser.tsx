2aa0ed8ffeca425148de1e474e5054117a9737f7
Karen Berba <berbak@oregonstate.edu>
2020-06-08 19:02:48 -0700

    Updated browser navigation icons and css

--- src/components/Browser.tsx
+++ src/components/Browser.tsx
@@ -5,90 +5,86 @@ import { useDispatch } from 'react-redux';
 import { Button } from '@material-ui/core';
 import { Card } from '../types';
 import { Actions, ActionKeys } from '../store/actions';
-// import { WebviewTag } from 'electron';
-import KeyboardArrowLeftIcon from '@material-ui/icons/KeyboardArrowLeft';
-import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
-import ReplayIcon from '@material-ui/icons/Replay';
 
 
 type BrowserState = {
-  history: URL[];
-  current: URL;
-  index: number;
+    history: URL[];
+    current: URL;
+    index: number;
 }
 
 export const BrowserComponent: React.FunctionComponent = () => {
-  // this is related to the URL bar
-  const [urlInput, setUrlInput] = useState('');
-  const [browserState, setBrowserState] = useState<BrowserState>({ history: [], current: new URL('https://epiclab.github.io/'), index: 0 })
+    // this is related to the URL bar
+    const [urlInput, setUrlInput] = useState('');
+    const [browserState, setBrowserState] = useState<BrowserState>({ history: [], current: new URL('https://epiclab.github.io/'), index: 0 })
 
-  const go = (e: React.KeyboardEvent) => {
-    if (e.keyCode != 13) return;
-    let history = browserState.history;
-    if (browserState.index > 0) {
-      history = history.slice(browserState.index);
-      setBrowserState({ ...browserState, index: 0 });
+    const go = (e: React.KeyboardEvent) => {
+        if (e.keyCode != 13) return;
+        let history = browserState.history;
+        if (browserState.index > 0) {
+            history = history.slice(browserState.index);
+            setBrowserState({ ...browserState, index: 0 });
+        }
+        setBrowserState({ ...browserState, current: new URL(urlInput), history: [new URL(urlInput), ...history] });
     }
-    setBrowserState({ ...browserState, current: new URL(urlInput), history: [new URL(urlInput), ...history] });
-  }
 
-  const backwards = () => {
-    if (browserState.index < browserState.history.length - 1) {
-      setBrowserState({ ...browserState, current: browserState.history[browserState.index + 1], index: browserState.index + 1 });
-      setUrlInput(browserState.current.toString());
+    const backwards = () => {
+        if (browserState.index < browserState.history.length - 1) {
+            setBrowserState({ ...browserState, current: browserState.history[browserState.index + 1], index: browserState.index + 1 });
+            setUrlInput(browserState.current.toString());
+        }
     }
-  }
 
-  const forwards = () => {
-    if (browserState.index > 0) {
-      setBrowserState({ ...browserState, current: browserState.history[browserState.index - 1], index: browserState.index - 1 });
-      setUrlInput(browserState.current.toString());
+    const forwards = () => {
+        if (browserState.index > 0) {
+            setBrowserState({ ...browserState, current: browserState.history[browserState.index - 1], index: browserState.index - 1 });
+            setUrlInput(browserState.current.toString());
+        }
     }
-  }
 
-  const reloadSite = () => {
-    setBrowserState({ ...browserState, current: browserState.current });
-  }
+    const reloadSite = () => {
+        setBrowserState({ ...browserState, current: browserState.current });
+    }
 
-  return (
-    <>
-      <div className='browser-topbar'>
-        <KeyboardArrowLeftIcon onClick={() => backwards()} fontSize="default" color="primary" />
-        <KeyboardArrowRightIcon onClick={() => forwards()} fontSize="default" color="primary" />
-        <ReplayIcon onClick={reloadSite} fontSize="small" color="primary" />
-        <input type="text" placeholder="URL" value={urlInput} onKeyDown={go} onChange={e => setUrlInput(e.target.value)} />
-      </div>
-      <div className='browser-content'>
-        <webview src={browserState.current.toString()} style={{ height: '205px', width: '200px' }} ></webview>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <div className='browser-topbar'>
+                <button className="arrow-left" onClick={() => backwards()} />
+                <button className="arrow-right" onClick={() => forwards()} />
+                <button className="refresh" onClick={reloadSite} />
+                <input className="url-bar-style" type="text" placeholder="URL" value={urlInput} onKeyDown={go} onChange={e => setUrlInput(e.target.value)} />
+            </div>
+            <div className='browser-content'>
+                <webview src={browserState.current.toString()} style={{ height: '228px', width: '200px' }}></webview>
+            </div>
+        </>
+    )
 }
 
 
 
 export const BrowserButton: React.FunctionComponent = () => {
-  const dispatch = useDispatch();
+    const dispatch = useDispatch();
 
-  const handleClick = async (e: React.MouseEvent) => {
-    e.preventDefault();
+    const handleClick = async (e: React.MouseEvent) => {
+        e.preventDefault();
 
-    const card: Card = {
-      id: v4(),
-      name: 'browser',
-      created: DateTime.local(),
-      modified: DateTime.local(),
-      captured: false,
-      left: 50,
-      top: 70,
-      type: 'Browser',
-      related: []
+        const card: Card = {
+            id: v4(),
+            name: 'browser',
+            created: DateTime.local(),
+            modified: DateTime.local(),
+            captured: false,
+            left: 50,
+            top: 70,
+            type: 'Browser',
+            related: []
+        };
+        const action: Actions = { type: ActionKeys.ADD_CARD, id: card.id, card: card };
+        dispatch(action);
     };
-    const action: Actions = { type: ActionKeys.ADD_CARD, id: card.id, card: card };
-    dispatch(action);
-  };
 
-  return (
-    <Button id='diffpicker-button' variant='contained' color='primary' onClick={e => handleClick(e)}>Open Browser...</Button>
-  );
+    return (
+        <Button id='diffpicker-button' variant='contained' color='primary' onClick={e => handleClick(e)}>Open Browser...</Button>
+    );
 }
