e362f0a9e771df180f8a97ccaaf82152a0291f45
Nicholas Nelson <nelsonni@oregonstate.edu>
2020-06-18 10:44:28 -0700

    FilePicker comments about React component lifecycle caveats no longer needed

--- src/components/FilePickerDialog.tsx
+++ src/components/FilePickerDialog.tsx
@@ -10,21 +10,7 @@ const FilePickerButton: React.FunctionComponent = () => {
   const handleClick = async (e: React.MouseEvent) => {
     e.preventDefault();
     const paths = await remote.dialog.showOpenDialog({ properties: ['openFile', 'openDirectory', 'multiSelections'] });
-
-    /**
-     * The Redux useSelector hook is synchronous with the React component lifecycle, therefore the value of the 
-     * filetypes useSelector hook has already been set before the handleClick function executes (since this function 
-     * is linked to the asynchronous onClick event). This poses a problem since the Redux useDispatch hook updates
-     * the Redux store, but the useSelector hook does not pick up on that state change until the next re-render cycle.
-     * 
-     * The metafile information needs to be present for loading a new Card component into the UI, therefore, we must
-     * update the metafile information in the React store on the same render cycle as the card state update.
-     * To handle updating metafile information and loading a card in one render, we cheat and take the metafile from 
-     * the Redux metafile update action directly.
-     */
-    if (!paths.canceled && paths.filePaths) paths.filePaths.map(async filePath => {
-      dispatch(loadCard({ filepath: filePath }));
-    });
+    if (!paths.canceled && paths.filePaths) paths.filePaths.map(async filePath => dispatch(loadCard({ filepath: filePath })));
   };
 
   return (
