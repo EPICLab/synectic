4ab5816cb17b8bfef3ffe2a0744b3c08efa36b29
Nicholas Nelson <nelsonni@oregonstate.edu>
2020-09-08 14:52:02 -0700

    extractFieldMap added to compliment extractFieldArray for mocked Redux stores

--- __test__/__mocks__/reduxStoreMock.ts
+++ __test__/__mocks__/reduxStoreMock.ts
@@ -40,12 +40,26 @@ export const mockStore = (initialState: RootState) => createMockStore(createStat
  * Exposes an array of field objects found in the state of a mocked Redux store composed on key-value map objects, using 
  * type inference and conditional typing to expose the value object types. The fields in a mocked Redux store (accessed by
  * calling `store.getState().fieldName`) are of the type `DeepPartial<T> | undefined`, which requires filtering out undefined
- * from both the store field and the deeper fields within the key-value mapj. Also, since we need the type of values in the
- * key-value map we rely on the `KeyVal` conditional type.
+ * from both the store field and the deeper fields within the key-value map. Also, since we need the type of values in the
+ * key-value map we rely on the `KeyVal` conditional type to infer value types.
  * @param map The key-value map object contained in a mocked Redux store.
  * @return A typed array of values stored in the field within the mocked Redux store.
  */
 export const extractFieldArray = <T extends Record<string, KeyVal<T>>>(map: DeepPartial<T> | undefined): KeyVal<T>[] => {
   if (map) return Object.values(map as T);
-  throw new Error('exposeStoreField() cannot resolve undefined');
+  throw new Error('exposeFieldArray() cannot resolve undefined');
+}
+
+/**
+ * Exposes a map of field objects found in the state of a mocked Redux store composed on key-value map objects, using
+ * type inference to expose the correct record types. The fields in a mocked Redux store (accessed by calling 
+ * `store.getState().fieldName`) are of the type `DeepPartial<T> | undefined`, which requires filtering out undefined
+ * from both the store field and the deeper fields within the key-value map. Also, since we need the type of values in the
+ * key-value map we rely on the `KeyVal` conditional type to infer value types.
+ * @param map The key-value map object contained in a mocked Redux store.
+ * @return A typed map stored in the field within the mocked Redux store.
+ */
+export const extractFieldMap = <T extends Record<string, KeyVal<T>>>(map: DeepPartial<T> | undefined): Record<string, KeyVal<T>> => {
+  if (map) return (map as T);
+  throw new Error('exposeFieldMap() cannot resolve undefined');
 }
