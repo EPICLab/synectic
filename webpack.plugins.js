3a5ec2ff74eca8e781136b6addd8a7cdaa7b0ac2
Nicholas Nelson <nelsonni@oregonstate.edu>
2021-01-05 18:11:46 -0800

    Remove redundant copy-webpack-plugin dependency (#266)

--- webpack.plugins.js
+++ webpack.plugins.js
@@ -1,14 +1,8 @@
 const path = require('path');
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = [
   new ForkTsCheckerWebpackPlugin({
     async: false
   }),
-  new CopyWebpackPlugin({
-    patterns: [
-      { from: 'src/assets', to: 'assets' }
-    ]
-  })
 ];
