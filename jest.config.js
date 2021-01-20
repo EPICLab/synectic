7e9459047fa78ee9a0043a031d9812b248e31b5f
Nicholas Nelson <nelsonni@oregonstate.edu>
2021-01-19 18:28:37 -0800

    tribou/jest-serializer-path added to normalize paths across all platforms in Jest snapshots

--- jest.config.js
+++ jest.config.js
@@ -9,7 +9,10 @@ module.exports = {
   },
   preset: 'ts-jest',
   roots: ['<rootDir>/__test__'],
-  snapshotSerializers: ['enzyme-to-json/serializer'],
+  snapshotSerializers: [
+    'enzyme-to-json/serializer',
+    'jest-serializer-path'
+  ],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
   moduleNameMapper: {
     "ace-builds": "<rootDir>/node_modules/ace-builds",
