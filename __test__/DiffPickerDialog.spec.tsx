7ed903b8cbb0e098fdcbbeca60d5a1598cd339a0
Nicholas Nelson <nelsonni@oregonstate.edu>
2021-03-03 21:48:02 -0800

    DiffPickerDialog button logic subsumed by dialog component

--- __test__/DiffPickerDialog.spec.tsx
+++ __test__/DiffPickerDialog.spec.tsx
@@ -1,13 +1,13 @@
 import '@testing-library/jest-dom';
 import React from 'react';
 import { render, fireEvent, screen } from '@testing-library/react';
-import { act } from 'react-dom/test-utils';
+// import { act } from 'react-dom/test-utils';
 import { v4 } from 'uuid';
 import { DateTime } from 'luxon';
 
-import { wrapInReduxContext } from './__mocks__/dndReduxMock';
 import { mockStore, extractFieldArray } from './__mocks__/reduxStoreMock';
-import DiffPickerButton, { DiffPickerDialog } from '../src/components/DiffPickerDialog';
+import DiffPickerDialog from '../src/components/DiffPickerDialog';
+import { wrapInReduxContext } from './__mocks__/dndReduxMock';
 
 describe('DiffPickerDialog', () => {
   /**
@@ -55,221 +55,169 @@ describe('DiffPickerDialog', () => {
     filetypes: {},
     metafiles: {},
     repos: {},
-    errors: {}
-  });
-
-  it('DiffPickerDialog closes when escape key is pressed', () => {
-    const onClose = jest.fn();
-    const cards = extractFieldArray(store.getState().cards);
-    render(<DiffPickerDialog open={true} options={cards} onClose={onClose} />);
-    expect(screen.queryAllByRole('dialog')[0]).toBeInTheDocument();
-
-    const dialog = screen.queryAllByRole('dialog')[0];
-    fireEvent.keyDown(dialog, { key: 'Escape', keyCode: 27, which: 27 });
-    expect(onClose).toHaveBeenCalled();
-  });
-
-  it('DiffPickerDialog closes when clicking outside of dialog', async () => {
-    const onClose = jest.fn();
-    const cards = extractFieldArray(store.getState().cards);
-    render(<DiffPickerDialog open={true} options={cards} onClose={onClose} />);
-    expect(screen.queryAllByRole('dialog')[0]).toBeInTheDocument();
-
-    const backdrop = document.querySelector('.MuiBackdrop-root');
-    if (backdrop) fireEvent.click(backdrop);
-    expect(onClose).toHaveBeenCalled();
-  });
-
-  it('DiffPickerDialog changes selected items for cards', () => {
-    const onClose = jest.fn();
-    const cards = extractFieldArray(store.getState().cards);
-
-    const { getAllByRole } = render(<DiffPickerDialog open={true} options={cards} onClose={onClose} />);
-    const trigger = getAllByRole('button')[0];
-
-    // open the select component
-    fireEvent.mouseDown(trigger);
-    const options = getAllByRole('option');
-    expect(options[0]).toHaveFocus();
-
-    // make a selection and close the select component
-    act(() => {
-      options[1].click();
-    });
-
-    expect(trigger).toHaveFocus();
-    expect(trigger).toHaveTextContent(new RegExp(`${cards[1].name}`, 'i'));
-  });
-
-  it('DiffPickerDialog returns UUIDs for selected cards on run', () => {
-    const onClose = jest.fn();
-    const cards = extractFieldArray(store.getState().cards);
-
-    const { getAllByRole } = render(<DiffPickerDialog open={true} options={cards} onClose={onClose} />);
-    const leftSelector = getAllByRole('button')[0];
-    const rightSelector = getAllByRole('button')[1];
-
-    const runDiffButton = screen.queryByText(/Run Diff/i);
-
-    // open the left select component
-    fireEvent.mouseDown(leftSelector);
-    const leftOptions = getAllByRole('option');
-    expect(leftOptions[0]).toHaveFocus();
-
-    // make selection for left card and close the component
-    act(() => {
-      leftOptions[1].click();
-    });
-
-    // open the right select component
-    fireEvent.mouseDown(rightSelector);
-    const rightOptions = getAllByRole('option');
-    expect(rightOptions[0]).toHaveFocus();
-
-    // make selection for right card and close
-    act(() => {
-      rightOptions[0].click();
-    });
-
-    // click the Run Diff... button
-    if (runDiffButton) fireEvent.click(runDiffButton);
-
-    // check the parameters of onClose fn to verify they match the card UUIDs
-    expect(onClose).toHaveBeenCalledWith(false, ['33', '14']);
-  });
-
-  it('DiffPickerDialog returns cancelled if no cards are selected', () => {
-    const onClose = jest.fn();
-    const cards = extractFieldArray(store.getState().cards);
-
-    render(<DiffPickerDialog open={true} options={cards} onClose={onClose} />);
-
-    const dialog = screen.queryAllByRole('dialog')[0];
-
-    // exit the dialog via escape key
-    fireEvent.keyDown(dialog, { key: 'Escape', keyCode: 27, which: 27 });
-
-    // check the parameters of onClose fn to verify that canceled parameter is true
-    expect(onClose).toHaveBeenCalledWith(true, ['', '']);
-  });
-
-  it('DiffPickerDialog returns cancelled if only the left card is selected', () => {
-    const onClose = jest.fn();
-    const cards = extractFieldArray(store.getState().cards);
-
-    const { getAllByRole } = render(<DiffPickerDialog open={true} options={cards} onClose={onClose} />);
-    const leftSelector = getAllByRole('button')[0];
-
-    const dialog = screen.queryAllByRole('dialog')[0];
-
-    // open the left select component
-    fireEvent.mouseDown(leftSelector);
-    const leftOptions = getAllByRole('option');
-    expect(leftOptions[0]).toHaveFocus();
-
-    // make selection for only the left card and close
-    act(() => {
-      leftOptions[1].click();
-    });
-
-    // exit the dialog via escape key    
-    fireEvent.keyDown(dialog, { key: 'Escape', keyCode: 27, which: 27 });
-
-    // check the parameters of onClose fn to verify that canceled parameter is true
-    expect(onClose).toHaveBeenCalledWith(true, ['', '']);
-  });
-
-  it('DiffPickerDialog returns cancelled if only the right card is selected', () => {
-    const onClose = jest.fn();
-    const cards = extractFieldArray(store.getState().cards);
-
-    const { getAllByRole } = render(<DiffPickerDialog open={true} options={cards} onClose={onClose} />);
-    const rightSelector = getAllByRole('button')[1];
-
-    const dialog = screen.queryAllByRole('dialog')[0];
-
-    // open the right select component
-    fireEvent.mouseDown(rightSelector);
-    const rightOptions = getAllByRole('option');
-    expect(rightOptions[0]).toHaveFocus();
-
-    // make selection for only the right card and close
-    act(() => {
-      rightOptions[1].click();
-    });
-
-    // exit the dialog via escape key
-    fireEvent.keyDown(dialog, { key: 'Escape', keyCode: 27, which: 27 });
-
-    // check the parameters of onClose fn to verify that canceled parameter is true
-    expect(onClose).toHaveBeenCalledWith(true, ['', '']);
-  });
-
-});
-
-describe('DiffPickerButton', () => {
-
-  const store = mockStore({
-    canvas: {
-      id: v4(),
-      created: DateTime.fromISO('1991-12-26T08:00:00.000-08:00'),
-      repos: [],
-      cards: ['14', '33'],
-      stacks: []
-    },
-    stacks: {},
-    cards: {
-      14: {
-        id: '14',
-        name: 'test.js',
-        type: 'Editor',
-        metafile: '243',
-        created: DateTime.fromISO('2019-01-21T08:14:52.181-08:00'),
-        modified: DateTime.fromISO('2019-11-19T19:22:47.572-08:00'),
-        left: 10,
-        top: 10
-      },
-      33: {
-        id: '33',
-        name: 'turtle.asp',
-        type: 'Editor',
-        metafile: '459',
-        created: DateTime.fromISO('1997-12-27T10:10:10.288-08:00'),
-        modified: DateTime.fromISO('1998-01-01T20:20:20.144-08:00'),
-        left: 27,
-        top: 105
+    modals: {
+      18: {
+        id: '18',
+        type: 'DiffPicker'
       }
-    },
-    filetypes: {},
-    metafiles: {},
-    repos: {},
-    errors: {}
+    }
   });
-  const DiffPickerContext = wrapInReduxContext(DiffPickerButton, store);
+  const DiffPickerContext = wrapInReduxContext(DiffPickerDialog, store);
 
-  it('DiffPickerButton displays DiffPickerDialog on button click', () => {
-    render(<DiffPickerContext />);
-    expect(screen.queryByText(/Run Diff/i)).not.toBeInTheDocument();
+  it('DiffPickerDialog closes when escape key is pressed', async () => {
+    const modals = extractFieldArray(store.getState().modals);
+    render(<DiffPickerContext {...modals[0]} />);
 
-    const button = screen.queryByText(/Diff\.\.\./i);
-    if (button) fireEvent.click(button);
-    expect(screen.getByText(/Run Diff/i)).toBeInTheDocument();
+    const dialog = await screen.findByText(/Select cards to diff/i);
+    expect(dialog).toBeInTheDocument();
+    screen.debug();
+
+    fireEvent.keyDown(dialog, {
+      key: 'Escape',
+      code: 'Escape',
+      keyCode: 27,
+      charCode: 27
+    });
+
+    expect(await screen.findByText(/Select cards to diff/i)).not.toBeInTheDocument();
+    screen.debug();
   });
 
-  it('DiffPickerButton resolves no Diff card on canceled run', () => {
-    render(<DiffPickerContext />);
+  // it('DiffPickerDialog closes when clicking outside of dialog', async () => {
+  //   const modals = extractFieldArray(store.getState().modals);
+  //   render(<DiffPickerContext {...modals[0]} />);
+  //   expect(screen.queryAllByRole('dialog')[0]).toBeInTheDocument();
 
-    // open dialog
-    const button = screen.queryByText(/Diff\.\.\./i);
-    if (button) fireEvent.click(button);
+  //   const dialog = screen.queryAllByRole('dialog')[0];
+  //   const backdrop = document.querySelector('.MuiBackdrop-root');
+  //   if (backdrop) fireEvent.click(backdrop);
+  //   expect(dialog).not.toBeInTheDocument();
+  // });
 
-    const dialog = screen.queryAllByRole('dialog')[0];
+  // it('DiffPickerDialog changes selected items for cards', () => {
+  //   const modals = extractFieldArray(store.getState().modals);
+  //   const cards = extractFieldArray(store.getState().cards);
 
-    // exit dialog via escape key
-    fireEvent.keyDown(dialog, { key: 'Escape', keyCode: 27, which: 27 });
+  //   const { getAllByRole } = render(<DiffPickerContext {...modals[0]} />);
+  //   const trigger = getAllByRole('button')[0];
 
-    // there should be no actions in the store
-    expect(store.getActions()).toEqual([]);
-  });
+  //   // open the select component
+  //   fireEvent.mouseDown(trigger);
+  //   const options = getAllByRole('option');
+  //   expect(options[0]).toHaveFocus();
+
+  //   // make a selection and close the select component
+  //   act(() => {
+  //     options[1].click();
+  //   });
+
+  //   expect(trigger).toHaveFocus();
+  //   expect(trigger).toHaveTextContent(new RegExp(`${cards[1].name}`, 'i'));
+  // });
+
+  // it('DiffPickerDialog returns UUIDs for selected cards on run', () => {
+  //   const modals = extractFieldArray(store.getState().modals);
+
+  //   const { getAllByRole } = render(<DiffPickerContext {...modals[0]} />);
+  //   const leftSelector = getAllByRole('button')[0];
+  //   const rightSelector = getAllByRole('button')[1];
+
+  //   const runDiffButton = screen.queryByText(/Run Diff/i);
+
+  //   // open the left select component
+  //   fireEvent.mouseDown(leftSelector);
+  //   const leftOptions = getAllByRole('option');
+  //   expect(leftOptions[0]).toHaveFocus();
+
+  //   // make selection for left card and close the component
+  //   act(() => {
+  //     leftOptions[1].click();
+  //   });
+
+  //   // open the right select component
+  //   fireEvent.mouseDown(rightSelector);
+  //   const rightOptions = getAllByRole('option');
+  //   expect(rightOptions[0]).toHaveFocus();
+
+  //   // make selection for right card and close
+  //   act(() => {
+  //     rightOptions[0].click();
+  //   });
+
+  //   // click the Run Diff... button
+  //   if (runDiffButton) fireEvent.click(runDiffButton);
+
+  //   // check that cards now contain a Diff card with the associated filenames
+  //   const cards = extractFieldArray(store.getState().cards);
+  //   expect(cards.find(c => c.name === 'DIFF <turtle.asp on unknown, test.js on unknown>')).toHaveLength(1);
+  // });
+
+  // it('DiffPickerDialog returns cancelled if no cards are selected', () => {
+  //   const modals = extractFieldArray(store.getState().modals);
+
+  //   render(<DiffPickerContext {...modals[0]} />);
+
+  //   const dialog = screen.queryAllByRole('dialog')[0];
+
+  //   // exit the dialog via escape key
+  //   fireEvent.keyDown(dialog, { key: 'Escape', keyCode: 27, which: 27 });
+
+  //   // check that the dialog no longer exists in the document
+  //   expect(dialog).not.toBeInTheDocument();
+  // });
+
+  // it('DiffPickerDialog returns cancelled if only the left card is selected', () => {
+  //   const modals = extractFieldArray(store.getState().modals);
+
+  //   const { getAllByRole } = render(<DiffPickerContext {...modals[0]} />);
+  //   const leftSelector = getAllByRole('button')[0];
+
+  //   let dialog = screen.queryAllByRole('dialog')[0];
+
+  //   // open the left select component
+  //   fireEvent.mouseDown(leftSelector);
+  //   const leftOptions = getAllByRole('option');
+  //   expect(leftOptions[0]).toHaveFocus();
+
+  //   // make selection for only the left card and close
+  //   act(() => {
+  //     leftOptions[1].click();
+  //   });
+
+  //   // exit the dialog via escape key    
+  //   fireEvent.keyDown(dialog, { key: 'Escape', keyCode: 27, which: 27 });
+
+  //   dialog = screen.queryAllByRole('dialog')[0];
+
+  //   // check whether dialog is still displayed
+  //   expect(dialog).not.toBeInTheDocument();
+  // });
+
+  // it('DiffPickerDialog returns cancelled if only the right card is selected', () => {
+  //   const modals = extractFieldArray(store.getState().modals);
+
+  //   const { getAllByRole } = render(<DiffPickerContext {...modals[0]} />);
+  //   const rightSelector = getAllByRole('button')[1];
+
+  //   let dialog = screen.queryAllByRole('dialog')[0];
+
+  //   // open the right select component
+  //   fireEvent.mouseDown(rightSelector);
+  //   const rightOptions = getAllByRole('option');
+  //   expect(rightOptions[0]).toHaveFocus();
+
+  //   // make selection for only the right card and close
+  //   act(() => {
+  //     rightOptions[1].click();
+  //   });
+
+  //   // exit the dialog via escape key
+  //   fireEvent.keyDown(dialog, { key: 'Escape', keyCode: 27, which: 27 });
+
+  //   dialog = screen.queryAllByRole('dialog')[0];
+
+  //   // check whether dialog is still displayed
+  //   expect(dialog).not.toBeInTheDocument();
+  // });
 
 });
--- src/components/DiffPickerDialog.tsx
+++ src/components/DiffPickerDialog.tsx
@@ -1,74 +1,29 @@
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { ThunkDispatch } from 'redux-thunk';
-import { InputLabel, FormControl, Button, Dialog, Select, Input, MenuItem, DialogTitle } from '@material-ui/core';
+import * as MUI from '@material-ui/core';
 
-import type { UUID, Card } from '../types';
+import type { UUID, Modal } from '../types';
 import { RootState } from '../store/root';
-import { Action } from '../store/actions';
+import { Action, ActionKeys } from '../store/actions';
 import { getMetafile } from '../containers/metafiles';
 import { loadCard } from '../containers/handlers';
 
-type DialogProps = {
-  open: boolean;
-  options: Card[];
-  onClose: (canceled: boolean, selected: [UUID, UUID]) => Promise<void>;
-}
-
-export const DiffPickerDialog: React.FunctionComponent<DialogProps> = props => {
-  const [selectedLeft, setSelectedLeft] = useState<UUID>('');
-  const [selectedRight, setSelectedRight] = useState<UUID>('');
-
-  return (
-    <Dialog id='picker-dialog' role='dialog' open={props.open} onClose={() => props.onClose(true, ['', ''])}>
-      <div className='diff-container'>
-        <DialogTitle id='picker-dialog-title' style={{ gridArea: 'header' }}>Select cards to diff</DialogTitle>
-        <FormControl id='form-control-left' aria-label='Left Form' style={{ gridArea: 'left', width: 100 }}>
-          <InputLabel htmlFor='diff-card-select-left' id='diff-card-select-left-label'>Left</InputLabel>
-          <Select id='diff-card-select-left' aria-labelledby='diff-card-select-left-label' value={selectedLeft}
-            autoWidth={true} onChange={(e) => setSelectedLeft(e.target.value as UUID)} input={<Input />}>
-            {props.options.filter(card => card.type === 'Editor').map(card => (
-              <MenuItem key={card.id} value={card.id}>
-                {card.name} (uuid: ...{card.id.slice(-5)}, modified {card.modified.toRelative()})
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-        <img className='diff_icon' />
-        <FormControl id='form-control-right' aria-label='Right Form' style={{ gridArea: 'right', width: 100 }}>
-          <InputLabel htmlFor='dif-card-select-right' id='diff-card-select-right-label'>Right</InputLabel>
-          <Select labelId='diff-card-select-right' id='diff-card-select-right' value={selectedRight}
-            autoWidth={true} onChange={(e) => setSelectedRight(e.target.value as UUID)} input={<Input />}>
-            {props.options.filter(card => card.type === 'Editor').map(card => (
-              <MenuItem key={card.id} value={card.id}>
-                {card.name} (uuid: ...{card.id.slice(-5)}, modified {card.modified.toRelative()})
-              </MenuItem>
-            ))}
-          </Select>
-        </FormControl>
-        <Button style={{ gridArea: 'footer' }} id='diffpickerdialog-button' variant='contained' color='primary' onClick={() => {
-          props.onClose(false, [selectedLeft, selectedRight]);
-        }}>Run Diff</Button>
-      </div>
-    </Dialog >
-  );
-}
-
-
-const DiffPickerButton: React.FunctionComponent = () => {
-  const [open, setOpen] = useState(false);
+const DiffPickerDialog: React.FunctionComponent<Modal> = props => {
   const cards = useSelector((state: RootState) => state.cards);
   const metafiles = useSelector((state: RootState) => state.metafiles);
   const dispatch = useDispatch<ThunkDispatch<RootState, undefined, Action>>();
+  const [selectedLeft, setSelectedLeft] = useState<UUID>('');
+  const [selectedRight, setSelectedRight] = useState<UUID>('');
 
   const handleClose = async (canceled: boolean, selected: [UUID, UUID]) => {
     if (canceled || !selected[0] || !selected[1]) {
-      setOpen(!open);
+      dispatch({ type: ActionKeys.REMOVE_MODAL, id: props.id });
       return;
     }
     const [left, right] = [cards[selected[0]], cards[selected[1]]];
     if (!left || !right) {
-      setOpen(!open);
+      dispatch({ type: ActionKeys.REMOVE_MODAL, id: props.id });
       return;
     }
 
@@ -77,16 +32,42 @@ const DiffPickerButton: React.FunctionComponent = () => {
     const diffCardName = `DIFF<${left.name} on ${leftMetafile?.branch},${right.name} on ${rightMetafile?.branch}>`;
     const diffMetafile = await dispatch(getMetafile({ virtual: { name: diffCardName, handler: 'Diff', targets: [left.id, right.id] } }));
     if (diffMetafile && diffMetafile.handler && leftMetafile && rightMetafile) dispatch(loadCard({ metafile: diffMetafile }));
-    setOpen(!open);
+    dispatch({ type: ActionKeys.REMOVE_MODAL, id: props.id });
   };
 
   return (
-    <>
-      <Button id='diffpicker-button' variant='contained' color='primary'
-        disabled={Object.values(cards).length < 2} onClick={() => setOpen(!open)}>Diff...</Button>
-      <DiffPickerDialog open={open} options={Object.values(cards)} onClose={handleClose} />
-    </>
+    <MUI.Dialog id='picker-dialog' role='dialog' open={true} onClose={() => handleClose(true, ['', ''])}>
+      <div className='diff-container'>
+        <MUI.DialogTitle id='picker-dialog-title' style={{ gridArea: 'header' }}>Select cards to diff</MUI.DialogTitle>
+        <MUI.FormControl id='form-control-left' aria-label='Left Form' style={{ gridArea: 'left', width: 100 }}>
+          <MUI.InputLabel htmlFor='diff-card-select-left' id='diff-card-select-left-label'>Left</MUI.InputLabel>
+          <MUI.Select id='diff-card-select-left' aria-labelledby='diff-card-select-left-label' value={selectedLeft}
+            autoWidth={true} onChange={(e) => setSelectedLeft(e.target.value as UUID)} input={<MUI.Input />}>
+            {Object.values(cards).filter(card => card.type === 'Editor').map(card => (
+              <MUI.MenuItem key={card.id} value={card.id}>
+                {card.name} (uuid: ...{card.id.slice(-5)}, modified {card.modified.toRelative()})
+              </MUI.MenuItem>
+            ))}
+          </MUI.Select>
+        </MUI.FormControl>
+        <img className='diff_icon' />
+        <MUI.FormControl id='form-control-right' aria-label='Right Form' style={{ gridArea: 'right', width: 100 }}>
+          <MUI.InputLabel htmlFor='dif-card-select-right' id='diff-card-select-right-label'>Right</MUI.InputLabel>
+          <MUI.Select labelId='diff-card-select-right' id='diff-card-select-right' value={selectedRight}
+            autoWidth={true} onChange={(e) => setSelectedRight(e.target.value as UUID)} input={<MUI.Input />}>
+            {Object.values(cards).filter(card => card.type === 'Editor').map(card => (
+              <MUI.MenuItem key={card.id} value={card.id}>
+                {card.name} (uuid: ...{card.id.slice(-5)}, modified {card.modified.toRelative()})
+              </MUI.MenuItem>
+            ))}
+          </MUI.Select>
+        </MUI.FormControl>
+        <MUI.Button style={{ gridArea: 'footer' }} id='diffpickerdialog-button' variant='contained' color='primary' onClick={() => {
+          handleClose(false, [selectedLeft, selectedRight]);
+        }}>Run Diff</MUI.Button>
+      </div>
+    </MUI.Dialog >
   );
 }
 
-export default DiffPickerButton;
+export default DiffPickerDialog;
