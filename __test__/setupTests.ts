c0a9ea2b8dbe80cf2d6baabeb54b1fc0a38e440e
Nicholas Nelson <nelsonni@oregonstate.edu>
2021-03-05 20:43:41 -0800

    Set AceEditor 'basePath' and 'modePath' for dynamic importing in Jest during testing; resolves #90

--- __test__/setupTests.ts
+++ __test__/setupTests.ts
@@ -1,1 +1,12 @@
 import '@testing-library/jest-dom/extend-expect';
+
+/**
+ * 'ace-builds/webpack-resolver' is for dynamically loading modes and themes, however, Jest does not rely on webpack
+ * for setting configurations and resolving paths. Therefore, resolving paths to modes and themes during testing will
+ * not work unless paths are directly set using `basePath`, `modePath`, and `themePath`. For more details regarding this
+ * problem and the steps used to resolve it, see https://github.com/EPICLab/synectic/issues/90. 
+ */
+import * as ace from 'ace-builds/src-noconflict/ace';
+ace.config.set('basePath', './node_modules/ace-builds/');
+ace.config.set('modePath', '');
+ace.config.set('themePath', '');
