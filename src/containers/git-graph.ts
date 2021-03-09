ed8744c46f6616688b7dda4d8a27a5ecb6cceb71
Nicholas Nelson <nelsonni@oregonstate.edu>
2021-03-09 09:59:03 -0800

    GitGraph layoutOptimizer and asyncGraphConstruction migrated to git-graph.ts

---
+++ src/containers/git-graph.ts
@@ -0,0 +1,109 @@
+import dagre from 'dagre';
+import { ArrowHeadType, Edge, FlowElement, Node, isNode, isEdge } from 'react-flow-renderer';
+
+import type { Repository } from '../types';
+import { currentBranch, getStatus } from '../containers/git';
+import { CommitInfo } from '../store/hooks/useGitHistory';
+import { flattenArray } from '../containers/flatten';
+import { colorSets } from '../containers/colors';
+
+const getGitNode = (commit: CommitInfo, branchHead: string | undefined): Node => ({
+  id: commit.oid,
+  type: 'gitNode',
+  data: {
+    text: '',
+    tooltip: '',
+    color: colorSets[5],
+    border: '',
+    branch: branchHead && branchHead === commit.oid ? `${commit.scope}/${commit.branch}` : undefined
+  },
+  position: { x: 0, y: 0 }
+});
+
+const getGitEdge = (commit: CommitInfo): Edge[] => {
+  return commit.commit.parent.map(parent => {
+    return {
+      id: `e${parent.slice(0, 7)}-${commit.oid.slice(0, 7)}`,
+      source: parent,
+      target: commit.oid,
+      arrowHeadType: ArrowHeadType.ArrowClosed
+    };
+  });
+};
+
+const getGitStaged = async (commit: CommitInfo, repo: Repository): Promise<(Node | Edge)[]> => {
+  const currentBranchStatus = await getStatus(repo.root);
+  if (currentBranchStatus && !['ignored', 'unmodified'].includes(currentBranchStatus)) {
+    return [{
+      id: `${commit.oid}*`,
+      type: 'gitNode',
+      data: {
+        text: '',
+        tooltip: '',
+        color: colorSets[5],
+        border: 'dashed',
+        opacity: '0.6',
+        branch: `${commit.scope}/${commit.branch}*`
+      },
+      position: { x: 0, y: 0 }
+    },
+    {
+      id: `e${commit.oid.slice(0, 7)}-${commit.oid.slice(0, 7)}*`,
+      source: commit.oid,
+      target: `${commit.oid}*`,
+      animated: true,
+      arrowHeadType: ArrowHeadType.ArrowClosed
+    }
+    ];
+  } else {
+    return [];
+  }
+};
+
+export const layoutOptimizer = (rfGraph: Array<FlowElement>): Array<FlowElement> => {
+  const graph = new dagre.graphlib.Graph();
+  graph.setGraph({ nodesep: 80 });
+  graph.setDefaultEdgeLabel(() => { return {}; });
+
+  rfGraph.filter(isNode).map(node => graph.setNode(node.id, { width: 5, height: 5 }));
+  rfGraph.filter(isEdge).map(edge => graph.setEdge(edge.source, edge.target));
+  dagre.layout(graph);
+
+  return rfGraph.map(els => {
+    if (isNode(els)) {
+      const graphNode = graph.node(els.id);
+      return { ...els, position: { x: graphNode.x, y: graphNode.y } };
+    }
+    return els;
+  });
+}
+
+export const asyncGraphConstruction = async (commits: Map<string, CommitInfo>, heads: Map<string, string>, repo: Repository)
+  : Promise<Array<FlowElement>> => {
+  const currentCommits = [...commits.values()].slice(Math.max(commits.size - 50, 0)) // limited to 50 most recent commits
+  const newElements = currentCommits.reduce((prev: Array<FlowElement>, curr: CommitInfo): Array<FlowElement> => {
+    const branchHead = heads.get(`${curr.scope}/${curr.branch}`);
+    const node: Node = getGitNode(curr, branchHead);
+    const edges: Edge[] = getGitEdge(curr);
+    return [node, ...prev, ...edges];
+  }, []);
+  const headsHashes = [...heads.values()];
+  const headCommits = currentCommits.filter(commit => headsHashes.includes(commit.oid));
+  // TODO: Until we have copied repositories that exist in a cache (probably in within .syn or .git directory), we will need to
+  // check through all the open cards on the canvas to determine if any of them are associated with a branch and have changes
+  // compared to the latest version in the branch. The following line has to wait until this is implemented:
+  //
+  // const staged = flattenArray(await Promise.all(headCommits.map(headCommit => getGitStaged(headCommit, props.repo))));
+  const currentBranchName = await currentBranch({ dir: repo.root.toString() });
+  const currentBranchHash = heads.get(`local/${currentBranchName}`);
+  const staged = flattenArray(await Promise.all(
+    headCommits
+      .filter(commit => commit.oid === currentBranchHash)
+      .map(currentBranchCommit => getGitStaged(currentBranchCommit, repo))
+  ));
+  const optimizedNewElements = layoutOptimizer([...newElements, ...staged]);
+  return optimizedNewElements;
+}
+
+
+
