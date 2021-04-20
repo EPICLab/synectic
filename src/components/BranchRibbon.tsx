75e9ab17688cb69b52d45cb924b4e67d1e1a7669
Nicholas Nelson <nelsonni@oregonstate.edu>
2021-04-19 22:33:23 -0700

    Extend BranchRibbon to include onClick events

--- src/components/BranchRibbon.tsx
+++ src/components/BranchRibbon.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export const BranchRibbon: React.FunctionComponent<{ branch?: string }> = props => {
+export const BranchRibbon: React.FunctionComponent<{ branch?: string, onClick?: () => void }> = props => {
   if (props.branch) {
     return (
-      <div className='branch-ribbon-container'>
+      <div className='branch-ribbon-container' onClick={props.onClick} >
         <div className={`branch-ribbon ${props.branch.length > 35 ? 'long' : ''}`}>
           {`Branch: ${props.branch}`}
         </div>
