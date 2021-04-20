67e760033023d1ebcfe83dedfe5ed90bb51ec57d
Nicholas Nelson <nelsonni@oregonstate.edu>
2021-04-19 22:31:11 -0700

    AtLeastOne utility type for restricting optional parameters to include at least one

--- src/containers/format.ts
+++ src/containers/format.ts
@@ -1,5 +1,7 @@
 import { flattenObject } from './flatten';
 
+export type AtLeastOne<T, U = { [K in keyof T]: Pick<T, K>; }> = Partial<T> & U[keyof U]; // see: https://stackoverflow.com/questions/48230773/how-to-create-a-partial-like-that-requires-a-single-property-to-be-set/48244432#48244432
+
 /**
  * Converts a JavaScript Object Notation (JSON) string into a typed object.
  * @param json A valid JSON string.
