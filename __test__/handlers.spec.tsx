ee7a863c649e91fd1ee8f134e14187a470960c2a
Sandy Updates <supdate@oregonstate.edu>
2021-07-29 11:18:18 -0700

    Current working tests with async reformatting.

--- __test__/handlers.spec.tsx
+++ __test__/handlers.spec.tsx
@@ -1,22 +1,26 @@
-import mock from 'mock-fs';
-
 import * as handlers from '../src/containers/handlers';
 import { ActionKeys } from '../src/store/actions';
+import { mock, MockInstance } from './__mocks__/mock-fs-promise';
+import mock2 from 'mock-fs';
 
-beforeEach(() => {
-  mock({
+let mockedInstance: MockInstance;
+
+beforeAll(async () => {
+  const instance = await mock({
     'foo/config/filetypes.json': '[{"filetype": "PHP", "handler": "Editor", "extensions": ["php", "phpt"]}]',
     'foo/data.php': 'sample data for supported filetype',
     'foo/data.azi': 'sample data for unsupported filetype'
   });
+  return mockedInstance = instance;
 });
+afterAll(() => mockedInstance.reset());
 
-afterEach(mock.restore);
+afterEach(mock2.restore);
 
 describe('handlers.importFiletypes', () => {
   it('importFiletypes returns Redux actions for adding filetypes', async () => {
     const filetypes = await handlers.importFiletypes();
-    mock.restore(); // required to prevent snapshot rewriting because of file watcher race conditions in Jest
+    mock2.restore(); // required to prevent snapshot rewriting because of file watcher race conditions in Jest
     expect(filetypes.length > 1).toBe(true);
     expect(filetypes[0].type).toBe(ActionKeys.ADD_FILETYPE);
   });
