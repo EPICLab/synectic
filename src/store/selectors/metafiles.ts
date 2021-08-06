7d105324d5a1cc67e0cb26ac1e1d8421e1c564ee
nelsonni <nelsonni@oregonstate.edu>
2021-08-06 14:59:30 -0700

    Expanded selectors for metafiles & repos

--- src/store/selectors/metafiles.ts
+++ src/store/selectors/metafiles.ts
@@ -1,4 +1,21 @@
+import { EntityState } from '@reduxjs/toolkit';
+import { PathLike } from 'fs-extra';
+import type { Metafile } from '../../types';
 import { RootState } from '../store';
-import { MetafilesState } from '../slices/metafiles';
 
-export const selectMetafiles = (state: RootState): MetafilesState => state.metafiles;
+export const selectMetafiles = (state: RootState): EntityState<Metafile> => state.metafiles;
+
+export const getMetafileByFilepath = (filepath: PathLike) => (state: RootState): Metafile | undefined => {
+    return Object.values(state.metafiles)
+        .find(m => m.path === filepath);
+}
+
+export const getMetafileByBranch = (filepath: PathLike, branch: string) => (state: RootState): Metafile | undefined => {
+    return Object.values(state.metafiles)
+        .find(m => m.path === filepath && m.branch === branch);
+}
+
+export const getMetafileByVirtual = (name: string, handler: string) => (state: RootState): Metafile | undefined => {
+    return Object.values(state.metafiles)
+        .find(m => m.name === name && m.handler === handler);
+}
--- src/store/selectors/repos.ts
+++ src/store/selectors/repos.ts
@@ -1,8 +1,14 @@
+import parsePath from 'parse-path';
+import type { UUID, Card, Repository } from '../../types';
 import { RootState } from '../store';
 import { ReposState } from '../slices/repos';
-import type { UUID, Card } from '../../types';
 
 export const selectRepos = (state: RootState): ReposState => state.repos;
+
+export const getRepoByName = (name: string, url?: parsePath.ParsedPath) => (state: RootState): Repository | undefined => {
+    return url ? Object.values(state.repos).find(r => r.name === name && r.url.href === url.href) :
+        Object.values(state.repos).find(r => r.name === name);
+}
 
 export const getCardsByRepo = (repo: UUID, branch?: string) => (state: RootState): Card[] => {
     return Object.values(state.cards)
