501a50e4d37197e9e2611c114cc641c689ce2ba6
Andrea Tongsak <andy.st2001@gmail.com>
2021-08-10 15:07:38 -0700

    components/GItGraph updated to use RTK hooks pt 1

--- src/components/GitGraph.tsx
+++ src/components/GitGraph.tsx
@@ -4,16 +4,17 @@ import ReactFlow, { addEdge, Connection, Edge, FlowElement, OnLoadFunc, OnLoadPa
 import type { UUID } from '../types';
 import { nodeTypes } from './GitNode';
 import { useGitHistory } from '../store/hooks/useGitHistory';
-import { useSelector } from 'react-redux';
-import { RootState } from '../store/root';
+import { RootState } from '../store/store';
 import { graphConstruction } from '../containers/git-graph';
+import { selectAllRepos } from '../store/selectors/repos';
+import { useAppSelector } from '../store/hooks';
 
 export const GitGraph: React.FunctionComponent<{ repo: UUID }> = props => {
   const [elements, setElements] = useState<Array<FlowElement>>([]);
   const [reactFlowState, setReactFlowState] = useState<OnLoadParams>();
   const onConnect = (params: Edge | Connection) => setElements((els) => addEdge(params, els));
   const onLoad: OnLoadFunc = (rf) => { setReactFlowState(rf) };
-  const repo = useSelector((state: RootState) => state.repos[props.repo]);
+  const repo = useAppSelector((state: RootState) => selectAllRepos.selectById(state, props.repo));
   const { commits, heads, update } = useGitHistory(repo);
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
