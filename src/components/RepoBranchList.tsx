ac250d76b9f9beea7662f405a06c242dacbdcd66
nelsonni <nelsonni@oregonstate.edu>
2021-08-10 15:32:24 -0700

    components/RepoBranchList updated to use RTK hooks

--- src/components/RepoBranchList.tsx
+++ src/components/RepoBranchList.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
 import TreeView from '@material-ui/lab/TreeView';
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 import ArrowRightIcon from '@material-ui/icons/ArrowRight';
 import ErrorIcon from '@material-ui/icons/Error';
 import { v4 } from 'uuid';
-
 import type { UUID } from '../types';
-import { RootState } from '../store/root';
+import { RootState } from '../store/store';
 import { removeDuplicates } from '../containers/format';
 import { StyledTreeItem } from './StyledTreeComponent';
 import { GitRepoIcon, GitBranchIcon } from './GitIcons';
 import { useBranchStatus } from '../store/hooks/useBranchStatus';
+import { useAppSelector } from '../store/hooks';
+import { selectAllRepos } from '../store/selectors/repos';
 
 const BranchStatus: React.FunctionComponent<{ repo: UUID, branch: string }> = props => {
   const { cards, modified, status } = useBranchStatus(props.repo, props.branch);
@@ -25,21 +25,18 @@ const BranchStatus: React.FunctionComponent<{ repo: UUID, branch: string }> = pr
 };
 
 const RepoStatusComponent: React.FunctionComponent<{ repoId: UUID }> = props => {
-  const repo = useSelector((state: RootState) => state.repos[props.repoId]);
-  const branches = removeDuplicates([...repo.local, ...repo.remote], (a: string, b: string) => a === b);
+  const repo = useAppSelector((state: RootState) => selectAllRepos.selectById(state, props.repoId));
+  const branches = repo ? removeDuplicates([...repo.local, ...repo.remote], (a: string, b: string) => a === b) : [];
 
-  return (
-    <StyledTreeItem key={repo.id} nodeId={repo.id}
-      labelText={repo.name}
-      labelIcon={GitRepoIcon}
-    >
+  return repo ? (
+    <StyledTreeItem key={repo.id} nodeId={repo.id} labelText={repo.name} labelIcon={GitRepoIcon}>
       {branches.map(branch => <BranchStatus key={v4()} repo={repo.id} branch={branch} />)}
-    </StyledTreeItem>
-  );
+    </StyledTreeItem >
+  ) : null;
 }
 
 export const VersionStatusComponent: React.FunctionComponent = () => {
-  const repos = useSelector((state: RootState) => Object.values(state.repos));
+  const repos = useAppSelector((state: RootState) => selectAllRepos.selectAll(state));
 
   return (
     <div className='version-tracker'>
