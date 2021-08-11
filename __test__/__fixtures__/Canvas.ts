15645ab6ab856a55f74ea23f4b22f210612f3a58
nelsonni <nelsonni@oregonstate.edu>
2021-08-11 15:03:58 -0700

    ReduxStore test fixtures updated to use RTK hooks

--- __test__/__fixtures__/Canvas.ts
+++
@@ -1,4 +0,0 @@
-import type { Canvas } from '../../src/types';
-import { testStore } from './ReduxStore';
-
-export const fullCanvas: Canvas = testStore.canvas;
--- __test__/__fixtures__/Metafile.ts
+++ __test__/__fixtures__/Metafile.ts
@@ -1,5 +1,5 @@
 import type { Metafile } from '../../src/types';
 import { testStore } from './ReduxStore';
 
-export const basicMetafile: Metafile = testStore.metafiles['821c9159-292b-4639-b90e-e84fc12740ee'];
-export const virtualMetafile: Metafile = testStore.metafiles['46ae0111-0c82-4ee2-9ee5-cd5bdf8d8a71'];
+export const basicMetafile = testStore.metafiles.entities['821c9159-292b-4639-b90e-e84fc12740ee'] as Metafile;
+export const virtualMetafile = testStore.metafiles.entities['46ae0111-0c82-4ee2-9ee5-cd5bdf8d8a71'] as Metafile;
