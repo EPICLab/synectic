1e46c8b1cb40f35ae45a5e21644e3d363927f0e6
Andrea Tongsak <andy.st2001@gmail.com>
2021-08-12 12:06:27 -0700

    Switch to RTK hooks started on app.spec.ts and format.spec.ts

---
+++ __test__/app.spec.ts
@@ -0,0 +1,35 @@
+// import rootReducer from '../src/store/store';
+import { DateTime } from 'luxon';
+
+import type { Card } from '../src/types';
+import { cardAdded } from '../src/store/slices/cards';
+import { createMockStore } from './__mocks__/reduxStoreMock';
+import { testStore } from './__fixtures__/ReduxStore';
+
+describe('App', () => {
+
+  const card: Card = {
+    id: 't829w0351',
+    name: 'card2',
+    type: 'Editor',
+    metafile: '65914371',
+    created: DateTime.fromISO('2014-04-09T08:14:02.371-08:00').valueOf(),
+    modified: DateTime.fromISO('2014-06-23T21:58:44.507-08:00').valueOf(),
+    left: 100,
+    top: 50
+  };
+
+  it('Redux store dispatches to resolvers', () => {
+    const store = createMockStore(testStore);
+    store.dispatch(cardAdded(card))
+    expect(Object.keys(store.getState().cards)).toHaveLength(2);
+    expect(store.getState().cards).toEqual(
+        expect.arrayContaining([
+            expect.objectContaining({
+                id: 't829w0351',
+                name: 'card2'
+            })
+        ])
+    );
+  });
+})
