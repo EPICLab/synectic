e40d26e08e2acf3093a718afe1a9a2d3abb48dee
nelsonni <nelsonni@oregonstate.edu>
2021-08-13 16:53:54 -0700

    ace-builds/webpack-resolver configured for test suite

---
+++ __test__/__mocks__/electronMock.ts
@@ -0,0 +1,10 @@
+const sampleFilePaths = ['./__test__/__mocks__/fileMock.js', './__test__/__mocks__/reduxStoreMock.ts'];
+const sampleFilePath = './__test__/__mocks__/fileMock.js';
+
+export const remote = {
+    dialog: {
+        // replace the showOpenDialog and showSaveDialog functions with spies which return a valid value
+        showOpenDialog: jest.fn().mockReturnValue(new Promise((resolve) => resolve({ canceled: false, filePaths: sampleFilePaths }))),
+        showSaveDialog: jest.fn().mockReturnValue(new Promise((resolve) => resolve({ canceled: false, filePath: sampleFilePath })))
+    }
+}
--- __test__/setupTests.ts
+++ __test__/setupTests.ts
@@ -1,4 +1,15 @@
 /* jest-dom adds custom jest matchers for asserting on DOM nodes, allowing for things like:
    expect(element).toHaveTextContent(/react/i)
-   learn more: https://github.com/testing-library/jest-dom */ 
+   learn more: https://github.com/testing-library/jest-dom */
 import '@testing-library/jest-dom/extend-expect';
+
+/**
+ * 'ace-builds/webpack-resolver' is for dynamically loading modes and themes, however, Jest does not rely on webpack
+ * for setting configurations and resolving paths. Therefore, resolving paths to modes and themes during testing will
+ * not work unless paths are directly set using `basePath`, `modePath`, and `themePath`. For more details regarding this
+ * problem and the steps used to resolve it, see https://github.com/EPICLab/synectic/issues/90. 
+ */
+import * as ace from 'ace-builds/src-noconflict/ace';
+ace.config.set('basePath', './node_modules/ace-builds/');
+ace.config.set('modePath', '');
+ace.config.set('themePath', '');
