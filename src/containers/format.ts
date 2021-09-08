03a10b094260206787b7cffbe736fc02f62a6623
Sandy Updates <supdate@oregonstate.edu>
2021-09-08 16:39:32 -0700

    JSDoc cleanup for containers/format

--- src/containers/format.ts
+++ src/containers/format.ts
@@ -1,6 +1,9 @@
 import { flattenObject } from './flatten';
 
-export type AtLeastOne<T, U = { [K in keyof T]: Pick<T, K>; }> = Partial<T> & U[keyof U]; // see: https://stackoverflow.com/questions/48230773/how-to-create-a-partial-like-that-requires-a-single-property-to-be-set/48244432#48244432
+/** Requires at least one type property, similar to `Partial<T>` but excludes the empty object.
+ * Inspired by: https://stackoverflow.com/questions/48230773/how-to-create-a-partial-like-that-requires-a-single-property-to-be-set/48244432#48244432
+ */
+export type AtLeastOne<T, U = { [K in keyof T]: Pick<T, K>; }> = Partial<T> & U[keyof U];
 
 /**
  * Converts a JavaScript Object Notation (JSON) string into a typed object.
