857597de3019cb249351a6ca5195ca8ab52e722d
Sandy Updates <supdate@oregonstate.edu>
2021-09-19 14:03:06 -0700

    Updated useWatcher for monitoring all filesystem events on files and directories (with automatic cleanup)

--- src/containers/hooks/useWatcher.ts
+++ src/containers/hooks/useWatcher.ts
@@ -5,6 +5,16 @@ import { FSWatcher, WatchOptions, watch } from 'chokidar';
 export type WatchEventType = 'add' | 'addDir' | 'change' | 'unlink' | 'unlinkDir' | 'ready' | 'raw' | 'error';
 export type WatchListener<T> = (event: WatchEventType, filename: T) => void;
 
+/**
+ * Custom React Hook for monitoring filesystem objects (files and directories) and triggering an event callback
+ * function when changes are detected. If a filepath is undefined, as in the case of virtual metafiles, then
+ * this hook becomes a no-op and will skip opening any FS watchers. This hook initializes FS watchers upon
+ * initialization, and will close all watchers prior based on the context closure of the calling component.
+ * @param filepath The relative or absolute path to a file or directory (recursively includes all subdirectories 
+ * and files) that should be watched for changes.
+ * @param eventHandler The event callback function that should be called on all detected change events.
+ * @param options Additional watcher options; see https://github.com/paulmillr/chokidar#api.
+ */
 const useWatcher = (filepath: PathLike, eventHandler: WatchListener<PathLike>, options?: WatchOptions): void => {
     // create a ref that caches the handler callback, and a ref to prevent race conditions on async requests
     const savedHandler = useRef<WatchListener<PathLike>>();
@@ -12,14 +22,13 @@ const useWatcher = (filepath: PathLike, eventHandler: WatchListener<PathLike>, o
     let watcher: FSWatcher | undefined;
 
     useEffect(() => {
-        // if filepath is undefined, then a virtual metafile is calling and no watcher is needed
         const registerWatcher = async () => {
             // update saved handler, if necessary
             if (savedHandler.current !== eventHandler) {
                 savedHandler.current = eventHandler;
             }
-
-            if (active.current) {
+            // if filepath is undefined, then a virtual metafile is calling and no watcher is needed
+            if (active.current && filepath !== undefined) {
                 // create watcher for file or directory (and all subdirectories)
                 watcher = watch(filepath.toString(), { ...options, ignoreInitial: true });
                 watcher.on('all', savedHandler.current);
@@ -29,7 +38,7 @@ const useWatcher = (filepath: PathLike, eventHandler: WatchListener<PathLike>, o
         // remove all watchers on cleanup; called just before component unmount
         return () => {
             active.current = false;
-            watcher.close();
+            if (filepath !== undefined) watcher.close();
         }
     }, []);
 }
