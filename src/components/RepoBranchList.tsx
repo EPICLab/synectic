23a38a990b1c4955fbacd4c2023960afef929f32
Sandy Updates <supdate@oregonstate.edu>
2021-09-19 14:10:12 -0700

    RepoBranchList component updated to use metafile selectors with filters instead of the deprecated useBranchStatus

--- src/components/RepoBranchList.tsx
+++ src/components/RepoBranchList.tsx
@@ -9,18 +9,22 @@ import { RootState } from '../store/store';
 import { removeDuplicates } from '../containers/format';
 import { StyledTreeItem } from './StyledTreeComponent';
 import { GitRepoIcon, GitBranchIcon } from './GitIcons';
-import { useBranchStatus } from '../containers/hooks/useBranchStatus';
 import { useAppSelector } from '../store/hooks';
-import { repoSelectors } from '../store/selectors/repos';
+import { getCardsByRepo, repoSelectors } from '../store/selectors/repos';
+import { metafileSelectors } from '../store/selectors/metafiles';
+
+const modifiedStatuses = ['modified', '*modified', 'deleted', '*deleted', 'added', '*added', '*absent', '*undeleted', '*undeletedmodified'];
 
 const BranchStatus: React.FunctionComponent<{ repo: UUID, branch: string }> = props => {
-  const { cards, modified, status } = useBranchStatus(props.repo, props.branch);
+  const cards = useAppSelector((state: RootState) => getCardsByRepo(state, props.repo, props.branch));
+  const metafiles = useAppSelector((state: RootState) => metafileSelectors.selectAll(state));
+  const modified = cards.map(c => metafiles.find(m => m.id === c.metafile)).filter(m => modifiedStatuses.includes(m.status));
 
   return (
     <StyledTreeItem key={`${props.repo}-${props.branch}`} nodeId={`${props.repo}-${props.branch}`}
       labelText={`${props.branch} [${modified.length}/${cards.length}]`}
       labelIcon={GitBranchIcon}
-      onClick={() => cards.map(async c => await status(c))}
+      onClick={() => console.log(`Branch ${props.branch}:\nmodified: ${JSON.stringify(modified, undefined, 2)}\ncards: ${JSON.stringify(cards, undefined, 2)}`)}
     />);
 };
 
