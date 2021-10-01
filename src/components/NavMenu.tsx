7b505e59effd8dba794df539096dbba78e6d3770
Nicholas Nelson <nelsonni@oregonstate.edu>
2021-10-01 10:26:57 -0700

    Combine NavItem components into NavMenu for simplicity

--- src/components/NavMenu.tsx
+++ src/components/NavMenu.tsx
@@ -5,7 +5,19 @@ import Grow from '@material-ui/core/Grow';
 import Paper from '@material-ui/core/Paper';
 import Popper from '@material-ui/core/Popper';
 import MenuList from '@material-ui/core/MenuList';
-import { NavItem, NavItemProps } from './NavItem';
+import MenuItem from '@material-ui/core/MenuItem';
+
+type NavItemProps = {
+  label: string;
+  disabled?: boolean;
+  click: (event: React.MouseEvent<HTMLLIElement, MouseEvent>) => void;
+}
+
+export const NavItem: React.FunctionComponent<NavItemProps> = ({ label, click, disabled }) => {
+  return (
+    <MenuItem onClick={click} disabled={disabled}>{label}</ MenuItem>
+  );
+}
 
 type NavMenuProps = {
   label: string;
