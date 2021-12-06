faedfae87c7a46ebaab0f7e1325ce40c8068f6de
Sandy Updates <supdate@oregonstate.edu>
2021-12-06 13:52:28 -0800

    RevertButton.tsx created

--- src/components/RevertButton.tsx
+++ src/components/RevertButton.tsx
@@ -1,32 +1,59 @@
-import React, { useContext } from 'react';
-import { History } from '@material-ui/icons';
+import React from 'react';
 import type { UUID } from '../types';
-import { fileSaveDialog } from '../containers/dialogs';
-import { writeFileAsync } from '../containers/io';
 import { useAppDispatch, useAppSelector } from '../store/hooks';
-import { addItemInArray, removeItemInArray } from '../store/immutables';
 import cardSelectors from '../store/selectors/cards';
 import metafileSelectors from '../store/selectors/metafiles';
-import { cardUpdated } from '../store/slices/cards';
-import { metafileUpdated } from '../store/slices/metafiles';
 import { RootState } from '../store/store';
-import { fetchVersionControl, isFileMetafile, isVirtualMetafile } from '../store/thunks/metafiles';
+import { isFilebasedMetafile, revertStagedChanges } from '../store/thunks/metafiles';
+import { Tooltip } from '@material-ui/core';
+import { History } from '@material-ui/icons';
+import { addItemInArray, removeItemInArray } from '../store/immutables';
+import { cardUpdated } from '../store/slices/cards';
 import { StyledIconButton } from './StyledIconButton';
-import { FSCache } from './Cache/FSCache';
-import { Button, Tooltip } from '@material-ui/core';
 
 const RevertButton: React.FunctionComponent<{ cardIds: UUID[] }> = props => {
     const cards = useAppSelector((state: RootState) => cardSelectors.selectByIds(state, props.cardIds));
     const metafiles = useAppSelector((state: RootState) => metafileSelectors.selectByIds(state, cards.map(c => c.metafile)));
-    const { cache } = useContext(FSCache);
-    const modified = metafiles.filter(m => m.path && m.content !== cache.get(m.path));
+    const unstaged = metafiles
+        .filter(m => m.status ? ['*absent', '*added', '*undeleted', '*modified', '*deleted'].includes(m.status) : false);
+    const staged = metafiles
+        .filter(m => m.status ? ['added', 'modified', 'deleted'].includes(m.status) : false);
     const dispatch = useAppDispatch();
 
-    const revert = async () => isFilebasedMetafile(props) ? await dispatch(revertStagedChanges(props)) : undefined;
-    const isVirtual = isVirtualMetafile(props);
-    const changes = props.path && props.status && ['*added', 'added', '*deleted', 'deleted', '*modified', 'modified'].includes(props.status) ? true : false;
+    const revert = async () => {
+        // map each staged and unstaged change
+        unstaged.filter(isFilebasedMetafile).map(async m => await dispatch(revertStagedChanges(m)));
+        staged.filter(isFilebasedMetafile).map(async m => await dispatch(revertStagedChanges(m)));
+    }
 
-    return (<Button onClick={revert} disabled={!changes}>Undo Changes</Button>)
+    const isCaptured = cards.length == 1 && cards[0].captured !== undefined;
+
+    const onHover = () => {
+        if (cards.length > 1) {
+            cards.filter(c => unstaged.find(m => c.metafile === m.id) || staged.find(m => c.metafile === m.id) ? true : false)
+                .map(c => dispatch(cardUpdated({ ...c, classes: addItemInArray(c.classes, 'selected') })));
+        }
+    }
+
+    const offHover = () => {
+        cards.map(c => dispatch(cardUpdated({ ...c, classes: removeItemInArray(c.classes, 'selected') })));
+    }
+
+    return (
+        <>
+            {(unstaged.length > 0 || staged.length > 0) && !isCaptured &&
+                <Tooltip title='Revert'>
+                    <StyledIconButton
+                        aria-label='revert'
+                        onClick={revert}
+                        onMouseEnter={onHover}
+                        onMouseLeave={offHover}
+                    >
+                        <History />
+                    </StyledIconButton>
+                </Tooltip>}
+        </>
+    )
 }
 
 export default RevertButton;
