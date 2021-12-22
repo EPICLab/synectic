a18b55edc44910b22da5dcc657351e1aa2750632
Nicholas Nelson <nelsonni@oregonstate.edu>
2021-12-22 13:09:57 -0800

    containers/hooks/useMap simplified for readability and maintainability

--- src/containers/hooks/useMap.ts
+++ src/containers/hooks/useMap.ts
@@ -24,35 +24,31 @@ type Return<K, V> = [Omit<Map<K, V>, 'set' | 'clear' | 'delete'>, Actions<K, V>]
 function useMap<K, V>(
     initialState: MapOrEntries<K, V> = new Map(),
 ): Return<K, V> {
-    const [map, setMap] = useState(new Map(initialState))
+    const [map, setMap] = useState(new Map(initialState));
 
     const actions: Actions<K, V> = {
         set: useCallback((key, value) => {
-            setMap(prev => {
-                const copy = new Map(prev)
-                copy.set(key, value)
-                return copy
-            })
+            setMap(prev => new Map(prev.set(key, value)))
         }, []),
 
         setAll: useCallback(entries => {
-            setMap(() => new Map(entries))
+            setMap(() => new Map(entries));
         }, []),
 
         remove: useCallback(key => {
             setMap(prev => {
-                const copy = new Map(prev)
-                copy.delete(key)
-                return copy
+                const copy = new Map(prev);
+                copy.delete(key);
+                return copy;
             })
         }, []),
 
         reset: useCallback(() => {
-            setMap(() => new Map())
+            setMap(() => new Map());
         }, []),
-    }
+    };
 
-    return [map, actions]
+    return [map, actions];
 }
 
 export default useMap;
