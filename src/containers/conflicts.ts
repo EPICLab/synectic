3b16a04f9025b611883cc61296b84f16384450b3
Nicholas Nelson <nelsonni@oregonstate.edu>
2022-01-04 09:29:48 -0800

    containers/conflicts utilities for checking individual filepaths and entire git projects for conflicts

---
+++ src/containers/conflicts.ts
@@ -0,0 +1,51 @@
+import * as path from 'path';
+import { PathLike } from 'fs-extra';
+import * as io from './io';
+import { getIgnore } from './git-plumbing';
+import { asyncFilter, removeUndefined } from './format';
+import { getRepoRoot } from './git-porcelain';
+import { Ignore } from 'ignore';
+
+export type Conflict = {
+    /** The relative or absolute path to the file containing conflicts. */
+    filepath: PathLike,
+    /** An array of indexed conflicts indicating the starting position of each conflict in the file. */
+    conflicts: number[]
+}
+
+export const checkFilepath = async (filepath: PathLike, ignoreManager?: Ignore): Promise<Conflict | undefined> => {
+    const root = await getRepoRoot(filepath);
+    if (root && !(await io.isDirectory(filepath))) {
+        const conflictPattern = /<<<<<<<[^]+?=======[^]+?>>>>>>>/gm;
+        const ignore = ignoreManager ? ignoreManager : (await getIgnore(root, true));
+        if (ignore.ignores(path.relative(root.toString(), filepath.toString()))) return undefined;
+        const content = await io.readFileAsync(filepath, { encoding: 'utf-8' });
+        const matches = Array.from(content.matchAll(conflictPattern));
+        const matching = { filepath: filepath, conflicts: removeUndefined(matches.map(m => m.index)) };
+        if (matching.conflicts.length > 0) return matching;
+    }
+    return undefined;
+}
+
+export const checkProject = async (root: PathLike | undefined): Promise<Conflict[]> => {
+    if (root) {
+        const conflictPattern = /<<<<<<<[^]+?=======[^]+?>>>>>>>/gm;
+        const ignore = (await getIgnore(root, true));
+
+        const paths = (await io.readDirAsyncDepth(root, 1))
+            .filter(p => p !== root)                                          // filter root filepath from results
+            .filter(p => !ignore.ignores(path.relative(root.toString(), p))); // filter based on git-ignore rules
+        const dirpaths = await asyncFilter(paths, async p => io.isDirectory(p));
+        const filepaths = paths.filter(p => !dirpaths.includes(p));
+
+        const matching = await Promise.all(filepaths.map(async f => {
+            const content = await io.readFileAsync(f, { encoding: 'utf-8' });
+            const matches = Array.from(content.matchAll(conflictPattern));
+            return { filepath: f, conflicts: removeUndefined(matches.map(m => m.index)) };
+        }));
+
+        return matching.filter(match => match.conflicts.length > 0);
+    } else {
+        return [];
+    }
+}
