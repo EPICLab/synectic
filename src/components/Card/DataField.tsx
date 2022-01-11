bf59f015697ffc1fc08d953eacd4f8c8fce08b51
Nicholas Nelson <nelsonni@oregonstate.edu>
2022-01-11 15:45:05 -0800

    DataField includes Tooltip for long text in props.field

--- src/components/Card/DataField.tsx
+++ src/components/Card/DataField.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Typography } from '@material-ui/core';
+import { Tooltip, Typography } from '@material-ui/core';
 
 const DataField: React.FunctionComponent<{ title: string, field: React.ReactNode, textField?: boolean }> = props => {
     return (
         <>
             <div className='title'><Typography variant='body2'>{props.title}:</Typography></div>
-            {props.textField ?
-                <div className='field'><Typography variant='body2'>{props.field}</Typography></div> :
+            {props.textField && props.field ?
+                <div className='field'><Tooltip title={props.field}><Typography variant='body2'>{props.field}</Typography></Tooltip></div> :
                 <div className='field'>{props.field}</div>
             }
         </>
