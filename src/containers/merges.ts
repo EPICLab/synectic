c851e9dd1a847a1ac02226f99043be2d5d4cab2b
Nicholas Nelson <nelsonni@oregonstate.edu>
2022-01-13 13:13:02 -0800

    containers/merges updated to use git-path.getWorktreePaths()

--- src/containers/merges.ts
+++ src/containers/merges.ts
@@ -3,10 +3,9 @@ import util from 'util';
 import * as fs from 'fs-extra';
 import * as path from 'path';
 import * as isogit from 'isomorphic-git';
-import { isLinkedWorktree } from './git-worktree';
-import { getRepoRoot, resolveRef } from './git-porcelain';
-import { readFileAsync } from './io';
+import { resolveRef } from './git-porcelain';
 import { branchLog } from './git-plumbing';
+import { getWorktreePaths } from './git-path';
 
 const promiseExec = util.promisify(exec);
 
@@ -30,19 +29,16 @@ export type MergeResult = {
  * Merge two branches; combining new commits from `compare` branch onto the `base` branch history. This function is a wrapper to the 
  * *git* command-line utility, which differs from most git commands in Synectic that rely upon the *isomorphic-git* module. This function
  * can handle merging across linked worktrees.
- * @param dir The working tree directory path.
+ * @param dir The worktree root directory path.
  * @param base The base branch where newly merged commits should be added.
  * @param compare The compare branch for deriving mergeable commits.
  * @returns A Promise object containing the merge results (per https://isomorphic-git.org/docs/en/merge), 
  * exec shell-command output (`stdout` and `stderr`), and a list of files containing conflicts (if a merge conflict prevented the merge).
  */
 export const merge = async (dir: fs.PathLike, base: string, compare: string): Promise<MergeResult> => {
-    const gitdir = path.join(dir.toString(), '.git');
-    const baseDir = (await isLinkedWorktree({ gitdir: gitdir })) ?
-        (await readFileAsync(gitdir, { encoding: 'utf-8' })).slice('gitdir: '.length).trim()
-        : (await getRepoRoot(dir));
+    const { worktreeLink } = await getWorktreePaths(dir);
 
-    if (!baseDir) return {
+    if (!worktreeLink) return {
         mergeStatus: {
             oid: '',
             alreadyMerged: false,
@@ -53,9 +49,9 @@ export const merge = async (dir: fs.PathLike, base: string, compare: string): Pr
         stderr: ''
     }
 
-    const commitDelta = await branchLog(baseDir, base, compare);
-    if (baseDir && commitDelta.length == 0) {
-        const oid = await resolveRef(baseDir, 'HEAD');
+    const commitDelta = await branchLog(worktreeLink, base, compare);
+    if (worktreeLink && commitDelta.length == 0) {
+        const oid = await resolveRef(worktreeLink, 'HEAD');
         return {
             mergeStatus: {
                 oid: oid ? oid : '',
@@ -71,12 +67,12 @@ export const merge = async (dir: fs.PathLike, base: string, compare: string): Pr
     let mergeResults: { stdout: string; stderr: string; } = { stdout: '', stderr: '' };
     let mergeError: ExecError | undefined;
     try {
-        mergeResults = await promiseExec(`git merge ${base} ${compare}`, { cwd: baseDir });
+        mergeResults = await promiseExec(`git merge ${base} ${compare}`, { cwd: worktreeLink.toString() });
     } catch (error) {
         mergeError = error as ExecError;
         const conflictPattern = /(?<=content conflict in ).*?(?=\n)/gm;
-        const conflicts = mergeError ? mergeError.stderr.match(conflictPattern)?.map(filename => path.resolve(baseDir, filename)) : [];
-        const oid = await resolveRef(baseDir, 'HEAD');
+        const conflicts = mergeError ? mergeError.stderr.match(conflictPattern)?.map(filename => path.resolve(worktreeLink.toString(), filename)) : [];
+        const oid = await resolveRef(worktreeLink, 'HEAD');
         return {
             mergeStatus: {
                 oid: oid ? oid : '',
@@ -89,7 +85,7 @@ export const merge = async (dir: fs.PathLike, base: string, compare: string): Pr
             stderr: mergeError.stderr
         };
     }
-    const oid = await resolveRef(baseDir, 'HEAD');
+    const oid = await resolveRef(worktreeLink, 'HEAD');
     return {
         mergeStatus: {
             oid: oid ? oid : '',
