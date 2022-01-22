c867c6aa2a1b1509624c6e761607a08adcc60db6
Nicholas Nelson <nelsonni@oregonstate.edu>
2022-01-21 21:11:51 -0800

    Simplified SourceControl and SourceFileComponent rely on new selectStagedByBranch and selectUnstagedByBranch metafile selectors

--- src/components/SourceControl/SourceControl.tsx
+++ src/components/SourceControl/SourceControl.tsx
@@ -1,49 +1,52 @@
-import React, { useMemo, useState } from 'react';
+import React from 'react';
 import { TreeView } from '@material-ui/lab';
 import type { Card, UUID } from '../../types';
 import { RootState } from '../../store/store';
 import { BranchRibbon } from './BranchRibbon';
 import { StyledTreeItem } from '../StyledTreeComponent';
 import { GitBranchIcon } from '../GitIcons';
-import { useAppDispatch, useAppSelector } from '../../store/hooks';
+import { useAppSelector } from '../../store/hooks';
 import metafileSelectors from '../../store/selectors/metafiles';
 import repoSelectors from '../../store/selectors/repos';
-import useDirectory from '../../containers/hooks/useDirectory';
-import { fetchVersionControl, FileMetafile, isFilebasedMetafile } from '../../store/thunks/metafiles';
-import { changedCheck, modifiedCheck, SourceFileComponent, stagedCheck } from './SourceFileComponent';
-import { add, remove } from '../../containers/git-plumbing';
-import { metafileUpdated } from '../../store/slices/metafiles';
+import { FileMetafile, isFileMetafile } from '../../store/thunks/metafiles';
+import { SourceFileComponent } from './SourceFileComponent';
 import branchSelectors from '../../store/selectors/branches';
+import { Add, Remove, SvgIconComponent, Warning } from '@material-ui/icons';
+
+export type SourceMotif = {
+  color: string | undefined,
+  icon: SvgIconComponent | undefined
+}
+
+export const conflictedCheck = (metafile: FileMetafile) => metafile.conflicts ? metafile.conflicts.length > 0 : false;
+export const stagedCheck = (metafile: FileMetafile) => metafile.status ? ['added', 'modified', 'deleted'].includes(metafile.status) : false;
+export const unstagedCheck = (metafile: FileMetafile) => metafile.status ? ['*absent', '*added', '*undeleted', '*modified', '*deleted'].includes(metafile.status) : false;
+
+export const getSourceMotif = (metafile: FileMetafile): SourceMotif => {
+  if (metafile.status) {
+    const conflicts = conflictedCheck(metafile);
+    const staged = stagedCheck(metafile);
+    const unstaged = unstagedCheck(metafile);
+    const icon = conflicts ? Warning : staged ? Remove : unstaged ? Add : undefined;
+    // #da6473 => red, #61aeee => blue, #d19a66 => orange
+    const color = conflicts ? '#da6473' : staged ? '#61aeee' : unstaged ? '#d19a66' : undefined;
+    return { color: color, icon: icon };
+  }
+  return { color: undefined, icon: undefined };
+};
 
 const SourceControl: React.FunctionComponent<{ sourceControlId: UUID }> = props => {
   const metafile = useAppSelector((state: RootState) => metafileSelectors.selectById(state, props.sourceControlId));
   const repo = useAppSelector((state: RootState) => repoSelectors.selectById(state, metafile && metafile.repo ? metafile.repo : ''));
   const branch = useAppSelector((state: RootState) => branchSelectors.selectById(state, metafile && metafile.branch ? metafile.branch : ''));
-  const { files, update } = useDirectory(metafile ? metafile.path : undefined);
-  const changed = useMemo(() => files.filter(f => changedCheck(f.status)), [files]);
-  const staged = useMemo(() => files.filter(f => stagedCheck(f.status)), [files]);
-  const modified = useMemo(() => files.filter(f => modifiedCheck(f.status)), [files]);
-  const dispatch = useAppDispatch();
-
-  const stage = async (metafile: FileMetafile) => {
-    await add(metafile.path);
-    const vcs = await dispatch(fetchVersionControl(metafile)).unwrap();
-    dispatch(metafileUpdated({ ...metafile, ...vcs }));
-    update();
-  };
-
-  const unstage = async (metafile: FileMetafile) => {
-    await remove(metafile.path);
-    const vcs = await dispatch(fetchVersionControl(metafile)).unwrap();
-    dispatch(metafileUpdated({ ...metafile, ...vcs }));
-    update();
-  };
+  const staged = useAppSelector((state: RootState) => metafileSelectors.selectStagedByBranch(state, branch ? branch.id : ''));
+  const unstaged = useAppSelector((state: RootState) => metafileSelectors.selectUnstagedByBranch(state, branch ? branch.id : ''));
 
   return (
     <>
       {branch ?
         <div className='list-component'>
-          <BranchRibbon branch={branch.ref} onClick={() => { console.log({ props, files, modified }) }} />
+          <BranchRibbon branch={branch.ref} onClick={() => { console.log({ metafile, branch, staged, unstaged }) }} />
           <TreeView
             expanded={[`${repo ? repo.name : ''}-${branch.ref}-staged`, `${repo ? repo.name : ''}-${branch.ref}-changed`]}
           >
@@ -53,20 +56,22 @@ const SourceControl: React.FunctionComponent<{ sourceControlId: UUID }> = props 
               labelInfoText={`${staged.length}`}
               labelIcon={GitBranchIcon}
             >
-              {repo ? staged.filter(isFilebasedMetafile).map(file =>
-                <SourceFileComponent key={file.path.toString()} repository={repo} update={() => unstage(file)} {...file} />)
+              {repo ?
+                staged.filter(isFileMetafile).map(file =>
+                  <SourceFileComponent key={file.path.toString()} metafileId={file.id} />)
                 : null
               }
             </StyledTreeItem>
             <StyledTreeItem key={`${repo ? repo.name : ''}-${branch.ref}-changed`}
               nodeId={`${repo ? repo.name : ''}-${branch.ref}-changed`}
               labelText='Changed'
-              labelInfoText={`${changed.length}`}
+              labelInfoText={`${unstaged.length}`}
               labelIcon={GitBranchIcon}
             >
-              {repo ? changed.filter(isFilebasedMetafile).map(file =>
-                <SourceFileComponent key={file.path.toString()} repository={repo} update={() => stage(file)} {...file} />)
-                : null
+              {repo ?
+                unstaged.filter(isFileMetafile).map(file =>
+                  <SourceFileComponent key={file.path.toString()} metafileId={file.id} />)
+                : <span>{JSON.stringify(unstaged)}</span>
               }
             </StyledTreeItem>
           </TreeView>
@@ -78,8 +83,7 @@ const SourceControl: React.FunctionComponent<{ sourceControlId: UUID }> = props 
 
 export const SourceControlReverse: React.FunctionComponent<Card> = props => {
   const metafile = useAppSelector((state: RootState) => metafileSelectors.selectById(state, props.metafile));
-  const repos = useAppSelector((state: RootState) => repoSelectors.selectAll(state));
-  const [repo] = useState(metafile?.repo ? repos.find(r => r.id === metafile.repo) : undefined);
+  const repo = useAppSelector((state: RootState) => repoSelectors.selectById(state, metafile && metafile.repo ? metafile.repo : ''));
   const branch = useAppSelector((state: RootState) => branchSelectors.selectById(state, metafile && metafile.branch ? metafile.branch : ''));
   return (
     <>
--- src/components/SourceControl/SourceFileComponent.tsx
+++ src/components/SourceControl/SourceFileComponent.tsx
@@ -1,64 +1,58 @@
 import React from 'react';
-import { InsertDriveFile, Add, Remove } from '@material-ui/icons';
-import type { GitStatus, Metafile, Repository } from '../../types';
+import { InsertDriveFile } from '@material-ui/icons';
+import type { UUID } from '../../types';
 import { StyledTreeItem } from '../StyledTreeComponent';
 import { extractFilename } from '../../containers/io';
-import { add, remove } from '../../containers/git-plumbing';
 import { removeUndefinedProperties } from '../../containers/format';
+import { useAppDispatch, useAppSelector } from '../../store/hooks';
+import { RootState } from '../../store/store';
+import metafileSelectors from '../../store/selectors/metafiles';
+import { fetchVersionControl, isFilebasedMetafile, isFileMetafile } from '../../store/thunks/metafiles';
+import { getSourceMotif, stagedCheck, unstagedCheck } from './SourceControl';
+import { add, remove } from '../../containers/git-plumbing';
+import { metafileUpdated } from '../../store/slices/metafiles';
 
-export const modifiedCheck = (status: GitStatus | undefined): boolean => {
-    if (!status) return false;
-    return !['absent', 'unmodified', 'ignored'].includes(status);
-}
+export const SourceFileComponent: React.FunctionComponent<{ metafileId: UUID }> = props => {
+    const metafile = useAppSelector((root: RootState) => metafileSelectors.selectById(root, props.metafileId));
+    const dispatch = useAppDispatch();
+    const motif = metafile && isFileMetafile(metafile) ? getSourceMotif(metafile) : undefined;
 
-export const changedCheck = (status: GitStatus | undefined): boolean => {
-    if (!status) return false;
-    return ['*absent', '*added', '*undeleted', '*modified', '*deleted'].includes(status);
-}
+    const stage = async () => {
+        if (metafile && isFileMetafile(metafile)) {
+            await add(metafile.path);
+            const vcs = await dispatch(fetchVersionControl(metafile)).unwrap();
+            dispatch(metafileUpdated({ ...metafile, ...vcs }));
+        }
+    };
 
-export const stagedCheck = (status: GitStatus | undefined): boolean => {
-    if (!status) return false;
-    return modifiedCheck(status) && !changedCheck(status);
-}
+    const unstage = async () => {
+        if (metafile && isFileMetafile(metafile)) {
+            await remove(metafile.path);
+            const vcs = await dispatch(fetchVersionControl(metafile)).unwrap();
+            dispatch(metafileUpdated({ ...metafile, ...vcs }));
+        }
+    };
 
-export type SourceFileProps = {
-    repository: Repository,
-    update: () => Promise<void>
-}
+    const handleLabelClick = async () => {
+        if (metafile && isFileMetafile(metafile)) {
+            stagedCheck(metafile) ? await unstage() : unstagedCheck(metafile) ? await stage() : undefined;
+        }
+    }
 
-export const SourceFileComponent: React.FunctionComponent<Metafile & SourceFileProps> = props => {
-
-    const colorFilter = (status: GitStatus | undefined) => (status && stagedCheck(status) ? '#61aeee'
-        : (status && changedCheck(status) ? '#d19a66' : undefined));
-
-    const iconFilter = (status: GitStatus | undefined) => (status && stagedCheck(status) ? Remove
-        : (status && changedCheck(status) ? Add : undefined));
-
-    const optionals = removeUndefinedProperties({ color: colorFilter(props.status), labelInfo: iconFilter(props.status) });
+    const optionals = removeUndefinedProperties({ color: motif?.color, labelInfo: motif?.icon });
 
     return (
         <>
-            {props.path ? <StyledTreeItem key={props.path.toString()} nodeId={props.path.toString()}
-                labelText={extractFilename(props.path)}
-                labelIcon={InsertDriveFile}
-                {...optionals}
-                enableHover={true}
-                labelInfoClickHandler={async () => {
-                    if (!props.status || !props.repo || !props.branch || !props.path) {
-                        console.log('cannot do anything with an unmodified file');
-                        return;
-                    }
-                    if (stagedCheck(props.status)) {
-                        console.log(`unstaging ${extractFilename(props.path)}...`);
-                        await remove(props.path);
-                        await props.update();
-                    } else if (modifiedCheck(props.status)) {
-                        console.log(`staging ${extractFilename(props.path)}...`);
-                        await add(props.path);
-                        await props.update();
-                    }
-                }} />
-                : null}
+            {metafile && isFilebasedMetafile(metafile) ?
+                <StyledTreeItem key={metafile.path.toString()}
+                    nodeId={metafile.path.toString()}
+                    labelText={extractFilename(metafile.path)}
+                    labelIcon={InsertDriveFile}
+                    {...optionals}
+                    enableHover={true}
+                    labelInfoClickHandler={handleLabelClick} />
+                : null
+            }
         </>
     );
 };
