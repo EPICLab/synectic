d7d13acfed63f962189e255bee5107eacf68ac64
Nicholas Nelson <nelsonni@oregonstate.edu>
2022-01-30 11:41:00 -0800

    Diff component updated to use metafile UUID instead of passing possibly stale Metafile objects via props

--- src/components/Card/ContentFront.tsx
+++ src/components/Card/ContentFront.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
 import type { Card } from '../../types';
-import Editor from '../Editor/Editor';
-import Diff from '../Diff/Diff';
-import Explorer from '../Explorer/Explorer';
-import SourceControl from '../SourceControl/SourceControl';
-import ConflictManager from '../SourceControl/ConflictManager';
 import Browser from '../Browser/Browser';
-import { ReposOverview } from '../SourceControl/ReposOverview';
-import { RootState } from '../../store/store';
-import { useAppSelector } from '../../store/hooks';
-import metafileSelectors from '../../store/selectors/metafiles';
+import ConflictManager from '../SourceControl/ConflictManager';
+import Diff from '../Diff/Diff';
+import Editor from '../Editor/Editor';
+import Explorer from '../Explorer/Explorer';
+import ReposOverview from '../SourceControl/ReposOverview';
+import SourceControl from '../SourceControl/SourceControl';
 
 const Content: React.FunctionComponent<Card> = card => {
-    const metafile = useAppSelector((state: RootState) => metafileSelectors.selectById(state, card.metafile));
-    if (!metafile) return null;
-
     switch (card.type) {
         case 'Editor':
             return (<Editor metafileId={card.metafile} />);
         case 'Diff':
-            return (<Diff metafile={metafile} />);
+            return (<Diff metafileId={card.metafile} />);
         case 'Explorer':
             return (<Explorer rootMetafileId={card.metafile} />);
         case 'SourceControl':
