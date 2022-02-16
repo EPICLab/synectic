bdb437403c579725ecd0ba5c1ed4d546c03c7fca
Nicholas Nelson <nelsonni@oregonstate.edu>
2022-02-15 21:01:48 -0800

    branchSelectors.selectByRepo prefers local branches over remote branches

--- src/store/selectors/branches.ts
+++ src/store/selectors/branches.ts
@@ -1,6 +1,5 @@
 import { createSelector, EntityId } from '@reduxjs/toolkit';
 import { PathLike } from 'fs-extra';
-import { removeDuplicates } from '../../containers/format';
 import type { Branch, Repository } from '../../types';
 import { branchesAdapter } from '../slices/branches';
 import { RootState } from '../store';
@@ -37,10 +36,14 @@ const selectByRepo = createSelector(
     (_state: RootState, _repo: Repository, dedup?: boolean) => dedup ? dedup : false,
     (branches, repo, dedup) => dedup
         ? branches.reduce((accumulator: Branch[], branch) => {
-            // prefer local branches over remote branches when deduplicating
-            if (repo.local.includes(branch.id)) return (accumulator.push(branch), accumulator);
-            else if (repo.remote.includes(branch.id) && !accumulator.some(b => b.ref === branch.ref))
+            if (repo.local.includes(branch.id)) {
+                // prefer local branches, remove matching remote branch if already added
+                return (accumulator.push(branch), accumulator)
+                    .filter(branch => !(branch.scope === 'remote' && branch.ref === branch.ref));
+            } else if (repo.remote.includes(branch.id) && !accumulator.some(b => b.scope === 'local' && b.ref === branch.ref)) {
+                // prefer local branches, only add remote if no matching local branch
                 return (accumulator.push(branch), accumulator);
+            }
             return accumulator;
         }, [])
         : branches.filter(branch => repo.local.includes(branch.id) || repo.remote.includes(branch.id))
