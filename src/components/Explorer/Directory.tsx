3799fcdc34aafc6b9c4de524dc00b8bcadbdbd9a
Nicholas Nelson <nelsonni@oregonstate.edu>
2022-03-07 13:19:38 -0800

    Explorer updated to self-contained module folder structure

--- src/components/Explorer/Directory.tsx
+++ src/components/Explorer/Directory.tsx
@@ -3,13 +3,12 @@ import FolderIcon from '@material-ui/icons/Folder';
 import FolderOpenIcon from '@material-ui/icons/FolderOpen';
 import { StyledTreeItem } from '../StyledTreeComponent';
 import { DirectoryMetafile, isDirectoryMetafile, isFileMetafile } from '../../store/thunks/metafiles';
-import { FileComponent } from "./FileComponent";
+import FileComponent from "./FileComponent";
 import { useAppSelector } from '../../store/hooks';
 import metafileSelectors from '../../store/selectors/metafiles';
 import { RootState } from '../../store/store';
 
-
-export const DirectoryComponent: React.FunctionComponent<DirectoryMetafile> = props => {
+const Directory = (props: DirectoryMetafile) => {
     const metafiles = useAppSelector((state: RootState) => metafileSelectors.selectByRoot(state, props.path));
     const [expanded, setExpanded] = useState(false);
 
@@ -22,8 +21,10 @@ export const DirectoryComponent: React.FunctionComponent<DirectoryMetafile> = pr
             labelIcon={expanded ? FolderOpenIcon : FolderIcon}
             onClick={clickHandle}
         >
-            {metafiles.filter(isDirectoryMetafile).map(dir => <DirectoryComponent key={dir.id} {...dir} />)}
+            {metafiles.filter(isDirectoryMetafile).map(dir => <Directory key={dir.id} {...dir} />)}
             {metafiles.filter(isFileMetafile).map(file => <FileComponent key={file.id} metafileId={file.id} />)}
         </StyledTreeItem>
     );
 };
+
+export default Directory;
