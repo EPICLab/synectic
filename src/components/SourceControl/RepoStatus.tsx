23c4b85f16f458c8fa8d8a01d2004dcf191a21e6
Nicholas Nelson <nelsonni@oregonstate.edu>
2022-03-07 13:26:09 -0800

    SourceControl updated to self-contained module folder structure

---
+++ src/components/SourceControl/RepoStatus.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { v4 } from 'uuid';
+import { RootState } from '../../store/store';
+import { StyledTreeItem } from '../StyledTreeComponent';
+import { GitRepoIcon } from '../GitIcons';
+import { useAppSelector } from '../../store/hooks';
+import branchSelectors from '../../store/selectors/branches';
+import { Repository } from '../../store/slices/repos';
+import BranchStatus from './BranchStatus';
+
+const RepoStatus = (props: { repo: Repository; }) => {
+    const branches = useAppSelector((state: RootState) => branchSelectors.selectByRepo(state, props.repo, true));
+
+    return (
+        <StyledTreeItem key={props.repo.id} nodeId={props.repo.id} labelText={props.repo.name} labelIcon={GitRepoIcon}>
+            {branches.filter(branch => branch.ref !== 'HEAD').map(branch => <BranchStatus key={v4()} repo={props.repo} branch={branch} />)}
+        </StyledTreeItem>
+    );
+};
+
+export default RepoStatus;
---
+++ src/components/SourceControl/SourceControlReverse.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { RootState } from '../../store/store';
+import { useAppSelector } from '../../store/hooks';
+import metafileSelectors from '../../store/selectors/metafiles';
+import repoSelectors from '../../store/selectors/repos';
+import branchSelectors from '../../store/selectors/branches';
+import DataField from '../Card/DataField';
+import { Card } from '../../store/slices/cards';
+
+
+const SourceControlReverse: React.FunctionComponent<Card> = props => {
+    const metafile = useAppSelector((state: RootState) => metafileSelectors.selectById(state, props.metafile));
+    const repo = useAppSelector((state: RootState) => repoSelectors.selectById(state, metafile && metafile.repo ? metafile.repo : ''));
+    const branch = useAppSelector((state: RootState) => branchSelectors.selectById(state, metafile && metafile.branch ? metafile.branch : ''));
+
+    return (
+        <>
+            <div className='buttons'></div>
+            <DataField title='Repo' textField field={repo ? repo.name : 'Untracked'} />
+            <DataField title='Branch' textField field={branch ? branch.ref : 'Untracked'} />
+        </>
+    );
+};
+
+export default SourceControlReverse;
