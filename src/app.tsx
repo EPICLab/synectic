de9e31669605e997506eb5090c16d7181b3d6280
Nicholas Nelson <nelsonni@oregonstate.edu>
2022-04-07 14:06:20 -0700

    importFiletypes thunk generalized to accept array of supported filetypes

--- src/app.tsx
+++ src/app.tsx
@@ -7,15 +7,17 @@ import './index.css';
 import './assets/style.css';
 import redux from './store/store';
 import { importFiletypes } from './store/thunks/filetypes';
+import filetypesJson from './containers/filetypes.json';
 import { PersistGate } from 'redux-persist/integration/react';
 import { FSCacheProvider } from './store/cache/FSCache';
 import Canvas from './components/Canvas';
+import { Filetype } from './store/slices/filetypes';
 
 const App = (): JSX.Element => {
 
   useEffect(() => {
     console.log(`process.env.NODE_ENV: ${process.env.NODE_ENV}`);
-    redux.store.dispatch(importFiletypes()); // load all supported filetype handlers into Redux store
+    redux.store.dispatch(importFiletypes(filetypesJson as Omit<Filetype, 'id'>[])); // load all supported filetype handlers into Redux store
     // redux.persistor.purge(); // remove all cached Redux data
   }, []); // run the effect only once; after the first render
 
