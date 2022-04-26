c234cc057d2c5b28698d6ae606ff5d79d5f200db
Nicholas Nelson <nelsonni@oregonstate.edu>
2022-04-25 20:53:49 -0700

    loadConflictManagers removed in favor of only filtering and resolving functions

--- src/containers/conflicts.ts
+++ src/containers/conflicts.ts
@@ -2,63 +2,12 @@ import * as path from 'path';
 import { PathLike } from 'fs-extra';
 import * as io from './io';
 import { getIgnore } from './git-plumbing';
-import { asyncFilter, removeUndefined } from './format';
+import { asyncFilter, isDefined, removeUndefined } from './format';
 import { Ignore } from 'ignore';
-import { createAsyncThunk } from '@reduxjs/toolkit';
-import { AppThunkAPI } from '../store/hooks';
-import { v4 } from 'uuid';
-import { fetchMetafile } from '../store/thunks/metafiles';
-import { DateTime } from 'luxon';
-import { loadCard } from '../store/thunks/filetypes';
-import { getBranchRoot, getRoot, getWorktreePaths } from './git-path';
+import { getRoot, getWorktreePaths } from './git-path';
+import { VersionedMetafile } from '../store/slices/metafiles';
 
-export type Conflict = {
-    /** The relative or absolute path to the file containing conflicts. */
-    filepath: PathLike,
-    /** An array of indexed conflicts indicating the starting position of each conflict in the file. */
-    conflicts: number[]
-}
-
-export const loadConflictManagers = createAsyncThunk<void, void, AppThunkAPI>(
-    'conflicts/loadConflictManagers',
-    async (_, thunkAPI) => {
-        const repos = removeUndefined(Object.values(thunkAPI.getState().repos.entities));
-        await Promise.all(repos.map(async repo => {
-            await Promise.all(repo.local.map(async branchId => {
-                const branch = thunkAPI.getState().branches.entities[branchId];
-                const root = branch ? await getBranchRoot(repo.root, branch.ref) : undefined;
-                const conflicts = await checkProject(root);
-                if (branch && root && conflicts.length > 0) {
-                    const { base, compare } = await resolveConflictBranches(root);
-                    const conflictManager = await thunkAPI.dispatch(fetchMetafile({
-                        virtual: {
-                            id: v4(),
-                            modified: DateTime.local().valueOf(),
-                            name: `Conflicts`,
-                            handler: 'ConflictManager',
-                            repo: repo.id,
-                            path: root,
-                            merging: { base: (base ? base : branch.ref), compare: compare }
-                        }
-                    })).unwrap();
-                    await thunkAPI.dispatch(loadCard({ metafile: conflictManager }));
-                }
-            }));
-        }));
-    }
-);
-
-export const resolveConflictBranches = async (root: PathLike): Promise<{ base: string | undefined, compare: string }> => {
-    const branchPattern = /(?<=^Merge branch(es)? .*)('.+?')+/gm;
-    const worktree = await getWorktreePaths(root);
-    const mergeMsg = worktree.gitdir ? await io.readFileAsync(path.join(worktree.gitdir.toString(), 'MERGE_MSG'), { encoding: 'utf-8' }) : '';
-    const match = mergeMsg.match(branchPattern);
-    return match
-        ? match.length === 2
-            ? { base: match[0].replace(/['"]+/g, ''), compare: match[1].replace(/['"]+/g, '') }
-            : { base: undefined, compare: match[0].replace(/['"]+/g, '') }
-        : { base: undefined, compare: '' };
-}
+export type Conflict = Pick<VersionedMetafile, 'path' | 'conflicts'>;
 
 export const checkFilepath = async (filepath: PathLike, ignoreManager?: Ignore): Promise<Conflict | undefined> => {
     const root = await getRoot(filepath);
@@ -68,11 +17,11 @@ export const checkFilepath = async (filepath: PathLike, ignoreManager?: Ignore):
         if (ignore.ignores(path.relative(root.toString(), filepath.toString()))) return undefined;
         const content = await io.readFileAsync(filepath, { encoding: 'utf-8' });
         const matches = Array.from(content.matchAll(conflictPattern));
-        const matching = { filepath: filepath, conflicts: removeUndefined(matches.map(m => m.index)) };
-        if (matching.conflicts.length > 0) return matching;
+        const conflicts: [number, number][] = removeUndefined(matches.map(m => isDefined(m.index) ? [m.index, m.index + m.toString().length] : undefined));
+        if (conflicts.length > 0) return { path: filepath, conflicts: conflicts };
     }
     return undefined;
-}
+};
 
 export const checkProject = async (root: PathLike | undefined): Promise<Conflict[]> => {
     if (root) {
@@ -88,11 +37,24 @@ export const checkProject = async (root: PathLike | undefined): Promise<Conflict
         const matching = await Promise.all(filepaths.map(async f => {
             const content = await io.readFileAsync(f, { encoding: 'utf-8' });
             const matches = Array.from(content.matchAll(conflictPattern));
-            return { filepath: f, conflicts: removeUndefined(matches.map(m => m.index)) };
+            const conflicts: [number, number][] = removeUndefined(matches.map(m => isDefined(m.index) ? [m.index, m.index + m.length] : undefined));
+            return { path: f, conflicts: conflicts };
         }));
 
         return matching.filter(match => match.conflicts.length > 0);
     } else {
         return [];
     }
-}
+};
+
+export const resolveConflictBranches = async (root: PathLike): Promise<{ base: string | undefined, compare: string }> => {
+    const branchPattern = /(?<=^Merge branch(es)? .*)('.+?')+/gm;
+    const worktree = await getWorktreePaths(root);
+    const mergeMsg = worktree.gitdir ? await io.readFileAsync(path.join(worktree.gitdir.toString(), 'MERGE_MSG'), { encoding: 'utf-8' }) : '';
+    const match = mergeMsg.match(branchPattern);
+    return match
+        ? match.length === 2
+            ? { base: match[0].replace(/['"]+/g, ''), compare: match[1].replace(/['"]+/g, '') }
+            : { base: undefined, compare: match[0].replace(/['"]+/g, '') }
+        : { base: undefined, compare: '' };
+};
