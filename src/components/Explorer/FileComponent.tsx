180483a3963d784a5a361307efc3c60536172c36
Nicholas Nelson <nelsonni@oregonstate.edu>
2022-04-25 21:04:01 -0700

    FileComponent hydrates only top-level metafiles with filebased and versioned fields

--- src/components/Explorer/FileComponent.tsx
+++ src/components/Explorer/FileComponent.tsx
@@ -1,31 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { InsertDriveFile as FileIcon, DeleteForever as Delete } from '@material-ui/icons';
 import { remove as removePath } from 'fs-extra';
 import metafileSelectors from '../../store/selectors/metafiles';
 import { RootState } from '../../store/store';
-import { loadCard } from '../../store/thunks/filetypes';
 import { extractFilename } from '../../containers/io';
 import { StyledTreeItem } from '../StyledTreeComponent';
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
-import { removeUndefinedProperties } from '../../containers/format';
-import { isFilebasedMetafile, isFileMetafile } from '../../store/thunks/metafiles';
+import { isDefined, removeUndefinedProperties } from '../../containers/format';
 import { getSourceMotif } from '../../containers/sourceMotif';
 import { UUID } from '../../store/types';
+import { isFilebasedMetafile, isFileMetafile } from '../../store/slices/metafiles';
+import { isHydrated, updateFilebasedMetafile } from '../../store/thunks/metafiles';
+import { createCard } from '../../store/thunks/cards';
+import { Skeleton } from '@material-ui/lab';
 
-const FileComponent = (props: { metafileId: UUID }) => {
-    const metafile = useAppSelector((root: RootState) => metafileSelectors.selectById(root, props.metafileId));
-    const dispatch = useAppDispatch();
+const FileComponent = (props: { metafile: UUID }) => {
+    const metafile = useAppSelector((root: RootState) => metafileSelectors.selectById(root, props.metafile));
+    const loaded = isDefined(metafile) && isFilebasedMetafile(metafile) && isHydrated(metafile);
     const motif = metafile && isFileMetafile(metafile) ? getSourceMotif(metafile) : undefined;
+    const optionals = removeUndefinedProperties({ color: motif?.color });
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        const asyncUpdate = async () => {
+            if (metafile && isFilebasedMetafile(metafile) && !isHydrated(metafile))
+                await dispatch(updateFilebasedMetafile(metafile));
+        };
+        asyncUpdate();
+    }, [metafile]);
 
     const handleClick = () => (metafile && metafile.status && ['*deleted', 'deleted'].includes(metafile.status)) ? null :
-        (metafile && metafile.path && dispatch(loadCard({ filepath: metafile.path })));
-
-    const optionals = removeUndefinedProperties({ color: motif?.color });
+        (metafile && metafile.path && dispatch(createCard({ path: metafile.path })));
 
     // expects a TreeView parent to encompass the StyledTreeItem below
     return (
         <>
-            {metafile && metafile.path &&
+            {loaded ?
                 <StyledTreeItem key={metafile.id} nodeId={metafile.id}
                     labelText={extractFilename(metafile.path)}
                     {...optionals}
@@ -40,7 +50,7 @@ const FileComponent = (props: { metafileId: UUID }) => {
                     labelIcon={FileIcon}
                     enableHover={true}
                     onClick={handleClick} />
-            }
+                : <Skeleton variant='text' aria-label='loading' />}
         </>
     );
 };
