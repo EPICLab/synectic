1d4e992406636481cfbe90f50e74fb90eb679af8
Nicholas Nelson <nelsonni@oregonstate.edu>
2022-04-25 20:53:02 -0700

    Simplified fileOpenDialog and fileSaveDialog using new Metafile functions

--- src/containers/dialogs.ts
+++ src/containers/dialogs.ts
@@ -2,12 +2,12 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import { ipcRenderer } from 'electron';
 import { join } from 'path';
 import { AppThunkAPI } from '../store/hooks';
-import { Metafile, metafileUpdated } from '../store/slices/metafiles';
-import { loadCard } from '../store/thunks/filetypes';
+import { isFilebasedMetafile, Metafile, metafileUpdated } from '../store/slices/metafiles';
+import { createCard } from '../store/thunks/cards';
 import { writeFileAsync } from './io';
-import { removeUndefined } from './format';
-import { fetchMetafile, fetchVersionControl, isFilebasedMetafile } from '../store/thunks/metafiles';
+import { updatedVersionedMetafile } from '../store/thunks/metafiles';
 import { fetchRepo } from '../store/thunks/repos';
+import metafileSelectors from '../store/selectors/metafiles';
 
 type PickerType = 'openFile' | 'openDirectory';
 
@@ -20,13 +20,12 @@ export const fileOpenDialog = createAsyncThunk<void, PickerType | void, AppThunk
     if (!paths.canceled && paths.filePaths) {
       if (paths.filePaths.length > 1) {
         /** Multiple filepaths loading asynchronously can cause a race condition where all filepaths appear to require a new repo, which 
-         * causes duplicated copies of the same repo to be added to the Redux store. Since multiple filepaths must all have the same root 
-         * parent directory, and therefore share the same repo, we can fix it by resolving the repo of the first path before loading any 
-         * other cards. */
-        const metafile = await thunkAPI.dispatch(fetchMetafile({ filepath: paths.filePaths[0] })).unwrap();
-        if (isFilebasedMetafile(metafile)) await thunkAPI.dispatch(fetchRepo(metafile));
+         * causes duplicated copies of the same repo to be added to the Redux store. Since selecting multiple files in the dialog results
+         * in filepaths that all have the same root parent directory, and therefore share the same repo, we can fix it by resolving the 
+         * repo of the first path before loading any other cards. */
+        await thunkAPI.dispatch(fetchRepo({ filepath: paths.filePaths[0] }));
       }
-      await Promise.all(paths.filePaths.map(async filePath => await thunkAPI.dispatch(loadCard({ filepath: filePath }))));
+      await Promise.all(paths.filePaths.map(async filePath => await thunkAPI.dispatch(createCard({ path: filePath }))));
     }
   }
 );
@@ -45,13 +44,9 @@ export const fileSaveDialog = createAsyncThunk<boolean, Metafile, AppThunkAPI>(
       await writeFileAsync(response.filePath, metafile.content);
       // update git info
       await Promise.all(
-        removeUndefined(Object.values(thunkAPI.getState().metafiles.entities))
-          .filter(m => m.path?.toString() === response.filePath)
+        metafileSelectors.selectByFilepath(thunkAPI.getState(), response.filePath)
           .filter(isFilebasedMetafile)
-          .map(async m => {
-            const vcs = await thunkAPI.dispatch(fetchVersionControl(m)).unwrap();
-            thunkAPI.dispatch(metafileUpdated({ ...m, ...vcs }));
-          })
+          .map(async m => await thunkAPI.dispatch(updatedVersionedMetafile(m)))
       );
       return true;
     }
