1cd93b961532bc7b227f8174e88bfb740889a24f
Nicholas Nelson <nelsonni@oregonstate.edu>
2022-05-10 09:21:24 -0700

    getRandomInt function for generating random numbers in a bounded range

--- src/containers/format.ts
+++ src/containers/format.ts
@@ -248,3 +248,14 @@ export const toArrayBuffer = (buf: Buffer): ArrayBuffer => {
   }
   return ab;
 };
+
+/**
+ * Generates a pseudo-random number bounded by inclusive minimum and maximum bounds.
+ * Reused from: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#getting_a_random_integer_between_two_values_inclusive
+ * @param min Minimum range value.
+ * @param max Maximum range value.
+ * @returns A pseudo-random number between `min` and `max` (inclusive).
+ */
+export const getRandomInt = (min: number, max: number): number => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
