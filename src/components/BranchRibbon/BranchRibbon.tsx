40b90a3a84a2cad877602ffd7fa33fa3444469fa
Nicholas Nelson <nelsonni@oregonstate.edu>
2022-05-18 16:35:33 -0700

    BranchRibbon loading Skeleton component is styled based on underlying branch-ribbon div

--- src/components/BranchRibbon/BranchRibbon.tsx
+++ src/components/BranchRibbon/BranchRibbon.tsx
@@ -1,29 +1,44 @@
+import { makeStyles } from '@material-ui/core';
 import { Skeleton } from '@material-ui/lab';
-import React, { useState } from 'react';
-import { getRandomInt } from '../../containers/utils';
+import React from 'react';
+import clsx from 'clsx';
 import { useAppSelector } from '../../store/hooks';
 import branchSelectors from '../../store/selectors/branches';
 import { Metafile } from '../../store/slices/metafiles';
 import { RootState } from '../../store/store';
 
+const useStyles = makeStyles({
+  branchRibbonContainer: {
+    backgroundColor: 'rgb(72, 74, 212, 1)'
+  },
+  branchRibbonPreview: {
+    backgroundColor: 'rgb(72, 74, 212, 0.7)'
+  }
+});
+
 const BranchRibbon = ({ metafile, onClick }: { metafile: Metafile | undefined, onClick?: () => void }) => {
-  const [random] = useState(getRandomInt(55, 90));
+  const classes = useStyles();
   const branch = useAppSelector((state: RootState) => branchSelectors.selectById(state, metafile && metafile.branch ? metafile.branch : ''));
 
   const ribbonText = `Branch: ${branch ? branch.ref : ''}`;
 
   return (
     <>
-      {metafile && metafile.loading ?
-        <div className='branch-ribbon-container' onClick={onClick} >
-          <Skeleton variant='text' aria-label='loading' width={`${random}%`} />
-        </div> :
-        branch ? <div className='branch-ribbon-container' onClick={onClick} >
-          <div className={`branch-ribbon ${ribbonText.length > 35 ? 'long' : ''}`}>
-            {ribbonText}
+      {metafile && metafile.loading
+        ? <div className={classes.branchRibbonPreview} onClick={onClick} >
+          <Skeleton variant='rect' aria-label='loading' animation='wave'>
+            <div className={'branch-ribbon'} style={{ width: 250 }}>
+              {ribbonText}
+            </div>
+          </Skeleton>
+        </div>
+        : branch ?
+          <div className={classes.branchRibbonContainer} onClick={onClick} >
+            <div className={clsx('branch-ribbon', { 'long': ribbonText.length > 35 })}>
+              {ribbonText}
+            </div>
           </div>
-        </div> :
-          null
+          : null
       }
     </>
   );
