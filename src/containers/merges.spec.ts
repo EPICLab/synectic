fd7b0568e3563f04edbe039f98ab13b2ae22971d
Nicholas Nelson <nelsonni@oregonstate.edu>
2022-07-05 15:56:28 -0700

    checkFilepath and checkProject migrated into containers/merges and switched to using native git commands (for increased performance)

---
+++ src/containers/merges.spec.ts
@@ -0,0 +1,43 @@
+import { mock, MockInstance } from '../test-utils/mock-fs';
+import { checkFilepath } from './merges';
+
+describe('containers/merges', () => {
+    let mockedInstance: MockInstance;
+
+    beforeAll(async () => {
+        const instance = await mock({
+            foo: {
+                'example.ts': 'import async from "typescript";\n\n<<<<<<< HEAD\nconst rand = Math.floor(Math.random() * 6) + 1;\n=======\nconst rand = Math.floor(Math.random() * 6) + 1;\n>>>>>>>\nconst double = rand * 2;\n<<<<<<< HEAD\nconst triple = rand * 3;\n=======\nconst triple = double + rand;\n>>>>>>>',
+                'bar.js': 'file contents',
+                '.git': {
+                    config: '',
+                    HEAD: 'refs/heads/main',
+                    refs: {
+                        remotes: {
+                            origin: {
+                                'HEAD': 'ref: refs/remotes/origin/main',
+                                'develop': 'ref: refs/remotes/origin/develop'
+                            }
+                        }
+                    }
+                }
+            }
+        });
+        return mockedInstance = instance;
+    });
+
+    afterAll(() => mockedInstance.reset());
+
+    it('checkFilepath resolves to undefined on non-conflicting file', async () => {
+        await expect(checkFilepath('foo/bar.js')).resolves.toBeUndefined();
+    });
+
+    it('checkFilepath resolves to a conflict array on conflicting file', async () => {
+        await expect(checkFilepath('foo/example.ts')).resolves.toStrictEqual(
+            expect.objectContaining({
+                path: 'foo/example.ts',
+                conflicts: [33, 183]
+            })
+        );
+    });
+});
