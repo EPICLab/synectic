70760c52a6695e8954dcd9ec54076c961f4f1991
Nicholas Nelson <nelsonni@oregonstate.edu>
2022-07-05 15:56:28 -0700

    checkFilepath and checkProject migrated into containers/merges and switched to using native git commands (for increased performance)

--- src/containers/merges.ts
+++ src/containers/merges.ts
@@ -3,11 +3,17 @@ import util from 'util';
 import * as fs from 'fs-extra';
 import * as path from 'path';
 import * as isogit from 'isomorphic-git';
-import { branchLog, resolveRef } from './git-plumbing';
-import { getWorktreePaths } from './git-path';
-import { extractStats } from './io';
+import * as io from './io';
+import { branchLog, getIgnore, resolveRef, resolveURL } from './git-plumbing';
+import { getBranchRoot, getWorktreePaths } from './git-path';
+import { checkout, getConfig } from './git-porcelain';
+import { VersionedMetafile } from '../store/slices/metafiles';
+import { Ignore } from 'ignore';
+import { isDefined, removeUndefined } from './utils';
 
 const promiseExec = util.promisify(exec);
+
+export type Conflict = Pick<VersionedMetafile, 'path' | 'conflicts'>;
 
 type ExecError = {
     killed: boolean,
@@ -16,14 +22,14 @@ type ExecError = {
     cmd: string,
     stdout: string,
     stderr: string
-}
+};
 
 export type MergeResult = {
     mergeStatus: isogit.MergeResult,
     mergeConflicts?: fs.PathLike[],
     stdout: string,
     stderr: string
-}
+};
 
 /**
  * Merge two branches; combining new commits from `compare` branch onto the `base` branch history. This function is a wrapper to the 
@@ -35,12 +41,14 @@ export type MergeResult = {
  * @returns A Promise object containing the merge results (per https://isomorphic-git.org/docs/en/merge), 
  * exec shell-command output (`stdout` and `stderr`), and a list of files containing conflicts (if a merge conflict prevented the merge).
  */
-export const merge = async (dir: fs.PathLike, base: string, compare: string): Promise<MergeResult> => {
+export const merge = async (dir: fs.PathLike, base: string, compare: string, onProgress?: isogit.ProgressCallback): Promise<MergeResult> => {
     const worktree = await getWorktreePaths(dir);
     const root = worktree.worktreeDir ? worktree.worktreeDir : worktree.dir;
 
-    const commitDelta = root ? await branchLog(root, base, compare) : [];
+    if (onProgress) await onProgress({ phase: `Merging: '${compare}' into '${base}'`, loaded: 0, total: 2 });
+    const commitDelta = root ? await branchLog(root, base, compare, onProgress) : [];
     if (root && commitDelta.length == 0) {
+        if (onProgress) await onProgress({ phase: `Already up to date: '${compare}' and '${base}' have the same commit history`, loaded: 1, total: 2 });
         const oid = worktree.dir ? await resolveRef({ dir: worktree.dir, ref: 'HEAD' }) : '';
         return {
             mergeStatus: {
@@ -49,22 +57,43 @@ export const merge = async (dir: fs.PathLike, base: string, compare: string): Pr
                 fastForward: false,
                 mergeCommit: false
             },
-            stdout: '',
+            stdout: 'Already up to date.',
             stderr: ''
         }
     }
 
     const branches = await isogit.listBranches({ fs: fs, dir: dir.toString() }); // branches found locally
+    const urlConfig = await getConfig({ dir: dir, keyPath: `remote.origin.url`, global: false });
+    const url = urlConfig.scope !== 'none' ? resolveURL(urlConfig.value) : '';
+    if (!branches.includes(base)) await checkout({ dir, ref: base, url, onProgress });
+
+    const branchRoot = await getBranchRoot(dir, base);
+    if (!branchRoot) {
+        if (onProgress) await onProgress({ phase: `Unable to locate branch root: '${base}' at ${dir.toString()}`, loaded: 1, total: 2 });
+        const oid = worktree.dir ? await resolveRef({ dir: worktree.dir, ref: 'HEAD' }) : '';
+        return {
+            mergeStatus: {
+                oid: oid ? oid : '',
+                alreadyMerged: false,
+                fastForward: false,
+                mergeCommit: false
+            },
+            stdout: '',
+            stderr: 'Unable to locate branch root.'
+        }
+    }
     let mergeResults: { stdout: string; stderr: string; } = { stdout: '', stderr: '' };
     let mergeError: ExecError | undefined;
+    if (onProgress) await onProgress({ phase: `Merging: ${compare} into ${base}`, loaded: 1, total: 2 });
     try {
-        mergeResults = await promiseExec(`git merge ${branches.includes(base) ? base : `origin/${base}`} ${branches.includes(compare) ? compare : `origin/${compare}`}`, { cwd: dir.toString() });
+        mergeResults = await promiseExec(`git -C ${branchRoot.toString()} merge ${base} ${branches.includes(compare) ? compare : `origin/${compare}`}`, { cwd: dir.toString() });
     } catch (error) {
         mergeError = error as ExecError;
         const conflictPattern = /(?<=conflict in ).*(?=\n)/gm;
         const mergeOutput = mergeError ? `${mergeError.stdout}\n${mergeError.stderr}` : '';
-        const conflicts = mergeOutput.match(conflictPattern)?.map(filename => worktree.dir ? path.resolve(worktree.dir.toString(), filename) : filename);
-        const oid = worktree.dir ? await resolveRef({ dir: worktree.dir, ref: 'HEAD' }) : '';
+        const conflicts = mergeOutput.match(conflictPattern)?.map(filename => path.resolve(branchRoot.toString(), filename));
+        const oid = worktree.dir ? await resolveRef({ dir: branchRoot, ref: 'HEAD' }) : '';
+        if (onProgress) await onProgress({ phase: `Halting merge: ${compare} into ${base}`, loaded: 2, total: 2 });
         return {
             mergeStatus: {
                 oid: oid ? oid : '',
@@ -77,7 +106,8 @@ export const merge = async (dir: fs.PathLike, base: string, compare: string): Pr
             stderr: mergeError.stderr
         };
     }
-    const oid = worktree.dir ? await resolveRef({ dir: worktree.dir, ref: 'HEAD' }) : '';
+    const oid = worktree.dir ? await resolveRef({ dir: branchRoot, ref: 'HEAD' }) : '';
+    if (onProgress) await onProgress({ phase: `Finishing merge: ${compare} into ${base}`, loaded: 2, total: 2 });
     return {
         mergeStatus: {
             oid: oid ? oid : '',
@@ -90,6 +120,48 @@ export const merge = async (dir: fs.PathLike, base: string, compare: string): Pr
     };
 }
 
+export const checkFilepath = async (filepath: fs.PathLike, ignoreManager?: Ignore): Promise<Conflict | undefined> => {
+    const { dir } = await getWorktreePaths(filepath);
+    if (dir && !(await io.isDirectory(filepath))) {
+        const conflictPattern = /<<<<<<<[^]+?=======[^]+?>>>>>>>/gm;
+        const ignore = ignoreManager ? ignoreManager : (await getIgnore(dir, true));
+        if (ignore.ignores(path.relative(dir.toString(), filepath.toString()))) return undefined;
+        const content = await io.readFileAsync(filepath, { encoding: 'utf-8' });
+        const matches = Array.from(content.matchAll(conflictPattern));
+        const conflicts: number[] = removeUndefined(matches.map(m => isDefined(m.index) ? m.index : undefined));
+        if (conflicts.length > 0) return { path: filepath, conflicts: conflicts };
+    }
+    return undefined;
+};
+
+/**
+ * Check for conflicts in a base branch after attempting to merge.
+ * @param dir The root directory of either the main worktree or linked worktree.
+ * @param branch The name of the branch to check against (i.e. the base branch).
+ * @returns A Promise object containing an array of conflict information found in the specified branch.
+ */
+export const checkProject = async (dir: fs.PathLike, branch: string): Promise<Conflict[]> => {
+    const branchRoot = await getBranchRoot(dir, branch);
+    if (!branchRoot) return [];
+
+    let checkResults: { stdout: string; stderr: string; } = { stdout: '', stderr: '' };
+    let checkError: ExecError | undefined;
+    try {
+        checkResults = await promiseExec(`git -C ${branchRoot.toString()} diff --check`);
+    } catch (error) {
+        checkError = error as ExecError;
+        checkResults = { stdout: checkError.stdout, stderr: checkError.stderr };
+    }
+    const conflictPattern = /(.+?)(?<=:)(\d)*(?=:)/gm; // Matches `<filename>:<position>` syntax, with a `:` positive lookbehind.
+    const conflictedFiles = new Map<string, number[]>();
+    checkResults.stdout.match(conflictPattern)?.forEach(match => {
+        const [filename, position] = match.split(':').slice(0, 2) as [string, number];
+        const existing = conflictedFiles.get(filename);
+        conflictedFiles.set(filename, existing ? [...existing, position] : [position]);
+    });
+    return Array.from(conflictedFiles).map(c => ({ path: c[0], conflicts: c[1] }));
+}
+
 /**
  * Abort the merging of branches; useful when merge conflicts arise and need to be backed out. This function is a wrapper to the 
  * *git* command-line utility, which differs from most git commands in Synectic that rely upon the *isomorphic-git* module. This function
@@ -98,7 +170,7 @@ export const merge = async (dir: fs.PathLike, base: string, compare: string): Pr
  */
 export const abortMerge = async (dir: fs.PathLike): Promise<void> => {
     const worktree = await getWorktreePaths(dir);
-    const stats = worktree.gitdir ? await extractStats(path.join(worktree.gitdir?.toString(), 'MERGE_HEAD')) : undefined;
+    const stats = worktree.gitdir ? await io.extractStats(path.join(worktree.gitdir?.toString(), 'MERGE_HEAD')) : undefined;
 
     if (stats) {
         try {
@@ -117,3 +189,15 @@ export const resolveMerge = async (dir: fs.PathLike, compareBranch: string): Pro
         console.error(error);
     }
 }
+
+export const resolveConflicts = async (root: fs.PathLike): Promise<{ base: string | undefined, compare: string }> => {
+    const branchPattern = /(?<=^Merge branch(es)? .*)('.+?')+/gm;
+    const { gitdir } = await getWorktreePaths(root);
+    const mergeMsg = gitdir ? await io.readFileAsync(path.join(gitdir.toString(), 'MERGE_MSG'), { encoding: 'utf-8' }) : '';
+    const match = mergeMsg.match(branchPattern);
+    return match
+        ? match.length === 2
+            ? { base: (match[0] as string).replace(/['"]+/g, ''), compare: (match[1] as string).replace(/['"]+/g, '') }
+            : { base: undefined, compare: (match[0] as string).replace(/['"]+/g, '') }
+        : { base: undefined, compare: '' };
+};
