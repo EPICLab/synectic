c47e0bf442e13588522338c02083b5b07c416683
Nicholas Nelson <nelsonni@oregonstate.edu>
2022-07-22 12:25:09 -0700

    Catch and update props.selectedBranch when a remote branch has been checked out into a local branch, which alters available options in BranchSelect

--- src/components/BranchSelect/BranchSelect.tsx
+++ src/components/BranchSelect/BranchSelect.tsx
@@ -1,4 +1,4 @@
-import React, { Dispatch, SetStateAction } from 'react';
+import React, { Dispatch, SetStateAction, useEffect } from 'react';
 import DropSelect from '../DropSelect';
 import { RootState } from '../../store/store';
 import { useAppSelector } from '../../store/hooks';
@@ -32,8 +32,18 @@ type BranchSelectProps = {
 
 const BranchSelect = (props: BranchSelectProps) => {
     const branches = useAppSelector((state: RootState) => branchSelectors.selectByRepo(state, props.repo ? props.repo : emptyRepo, true));
+    const branchLookup = useAppSelector((state: RootState) => branchSelectors.selectEntities(state));
     const predicate = props.optionsFilter ? props.optionsFilter : undefined; // Cannot descriminate types from variables; see https://github.com/microsoft/TypeScript/issues/10530
     const options = (predicate ? branches.filter(b => predicate(b)) : branches).map(b => { return { key: b.id, value: b.ref }; });
+
+    useEffect(() => {
+        if (props.selectedBranch !== '' && !options.find(o => o.key === props.selectedBranch)) {
+            // update selected branch when a local branch overrides a previously selected remote branch
+            const missingBranch = branchLookup[props.selectedBranch];
+            const updatedBranchOption = options.find(o => o.value === missingBranch?.ref);
+            if (updatedBranchOption) props.setSelectedBranch(updatedBranchOption.key);
+        }
+    }, [branchLookup, options, props]);
 
     return (<DropSelect label={props.label} target={props.selectedBranch} setTarget={props.setSelectedBranch} options={options} />);
 }
