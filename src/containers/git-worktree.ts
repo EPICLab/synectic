c4af8c52ee5ea9be184212dbf0edbce21123b6a0
Nicholas Nelson <nelsonni@oregonstate.edu>
2022-07-25 15:39:01 -0700

    Revert "Debug output for git-worktree.add when executed by git-porcelain.checkout"
    This reverts commit 4748a2caae68b4198b832dda0546667e37333e03.

--- src/containers/git-worktree.ts
+++ src/containers/git-worktree.ts
@@ -266,13 +266,7 @@ export const add = async (dir: fs.PathLike, worktreeDir: fs.PathLike, url: strin
 
   // // overwrite the internal git files in linked worktree to point to main worktree
   await fs.remove(worktreeGitdir);
-  console.log(`git-worktree.add => worktreeGitdir: ${worktreeGitdir}`);
-  try {
-    await io.writeFileAsync(worktreeGitdir, `gitdir: ${worktreeLink}\n`);
-  } catch (error) {
-    console.log(`git-worktree.add => error:`, error);
-  }
-  console.log(`git-worktree.add => worktreeGitdir: ${worktreeGitdir} successfully created [if no error]`);
+  await io.writeFileAsync(worktreeGitdir, `gitdir: ${worktreeLink}\n`);
   return;
 }
 
