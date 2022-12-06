adf84425ab6e056f5bf409f6e204e235943e09fa
Nicholas Nelson <nelsonni@oregonstate.edu>
2022-12-06 15:01:24 -0800

    Abort button enable via checks against merging branches instead of merging metafiles

--- src/components/Button/Abort.tsx
+++ src/components/Button/Abort.tsx
@@ -1,25 +1,28 @@
 import { IconButton, Tooltip } from '@material-ui/core';
 import { Cancel } from '@material-ui/icons';
 import React from 'react';
-import { checkUnmergedBranch, getBranchRoot, mergeInProgress } from '../../containers/git';
+import { checkUnmergedBranch, mergeInProgress } from '../../containers/git';
 import { isDefined } from '../../containers/utils';
 import { useAppDispatch, useAppSelector } from '../../store/hooks';
+import branchSelectors from '../../store/selectors/branches';
 import cardSelectors from '../../store/selectors/cards';
 import metafileSelectors from '../../store/selectors/metafiles';
-import repoSelectors from '../../store/selectors/repos';
-import { isMergingMetafile, Metafile } from '../../store/slices/metafiles';
-import { RootState } from '../../store/store';
+import { Branch, isMergingBranch } from '../../store/slices/branches';
+import { Metafile } from '../../store/slices/metafiles';
+import { updateBranch } from '../../store/thunks/branches';
 import { updateConflicted } from '../../store/thunks/metafiles';
 import { UUID } from '../../store/types';
 import { Mode, useIconButtonStyle } from './useStyledIconButton';
 
-export const isAbortable = (metafile: Metafile | undefined) => isDefined(metafile) && isMergingMetafile(metafile);
+export const isAbortable = (branch: Branch | undefined, metafile: Metafile | undefined) =>
+    isDefined(branch) && isMergingBranch(branch) &&
+    isDefined(metafile) && metafile.handler === 'Explorer';
 
 type AbortButtonProps = {
     cardId: UUID,
     enabled?: boolean,
     mode?: Mode
-}
+};
 
 /**
  * Button for aborting an in-progress merge that has halted due to conflicts.
@@ -31,25 +34,23 @@ type AbortButtonProps = {
  * @returns {React.Component} A React function component.
  */
 const AbortButton = ({ cardId, enabled = true, mode = 'light' }: AbortButtonProps) => {
-    const card = useAppSelector((state: RootState) => cardSelectors.selectById(state, cardId));
-    const metafile = useAppSelector((state: RootState) => metafileSelectors.selectById(state, card?.metafile ? card.metafile : ''));
-    const repo = useAppSelector((state: RootState) => repoSelectors.selectById(state, metafile?.repo ? metafile.repo : ''));
+    const card = useAppSelector(state => cardSelectors.selectById(state, cardId));
+    const metafile = useAppSelector(state => metafileSelectors.selectById(state, card?.metafile ? card.metafile : ''));
+    const branch = useAppSelector(state => branchSelectors.selectById(state, metafile?.branch ?? ''));
     const classes = useIconButtonStyle({ mode: mode });
     const dispatch = useAppDispatch();
 
     const abort = async (event: React.MouseEvent) => {
         event.stopPropagation(); // prevent propogating the click event to underlying components that might have click event handlers
-        if (repo && metafile?.merging?.base) {
-            const branchRoot = await getBranchRoot(repo.root, metafile.merging.base);
-            if (branchRoot) {
-                const conflicted = await checkUnmergedBranch(branchRoot, metafile.merging.base);
-                await mergeInProgress({ dir: branchRoot, action: 'abort' });
-                await dispatch(updateConflicted(conflicted));
-            }
+        if (isDefined(branch)) {
+            const conflicted = await checkUnmergedBranch(branch.root, branch.ref);
+            await mergeInProgress({ dir: branch.root, action: 'abort' });
+            await dispatch(updateConflicted(conflicted)); // update all conflicted Metafiles with new status
+            await dispatch(updateBranch(branch)); // update the Branch status and merging fields
         }
     }
 
-    return (enabled && isAbortable(metafile)) ? (
+    return (enabled && isAbortable(branch, metafile)) ? (
         <Tooltip title='Abort Merge'>
             <IconButton
                 className={classes.root}
