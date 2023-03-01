b0f1e4527d44905eaae443db4b94471c20c32742
Nicholas Nelson <nelsonni@oregonstate.edu>
2023-02-28 23:47:33 -0800

    fetchRepo simplified to check for existing branch with matching root before dispatching buildRepo with jumping through fetchMetafile and selectById first

--- src/store/thunks/repos.ts
+++ src/store/thunks/repos.ts
@@ -6,10 +6,10 @@ import { extractFilename } from '../../containers/io';
 import { ExactlyOne } from '../../containers/utils';
 import { createAppAsyncThunk } from '../hooks';
 import repoSelectors from '../selectors/repos';
-import { FilebasedMetafile, isFilebasedMetafile, isVersionedMetafile } from '../slices/metafiles';
+import { FilebasedMetafile, isVersionedMetafile } from '../slices/metafiles';
 import { repoAdded, Repository } from '../slices/repos';
 import { fetchBranches } from './branches';
-import { fetchMetafile, fetchParentMetafile } from './metafiles';
+import { fetchParentMetafile } from './metafiles';
 
 export const fetchRepo = createAppAsyncThunk<Repository | undefined, ExactlyOne<{ filepath: PathLike, metafile: FilebasedMetafile }>>(
     'repos/fetchRepo',
@@ -19,8 +19,8 @@ export const fetchRepo = createAppAsyncThunk<Repository | undefined, ExactlyOne<
         if (input.metafile) {
             // if metafile already has a repo UUID, check for matching repository
             let repo = input.metafile.repo ? repoSelectors.selectById(state, input.metafile.repo) : undefined;
-            const parent = !repo ? await thunkAPI.dispatch(fetchParentMetafile(input.metafile)).unwrap() : undefined;
             // otherwise if parent metafile already has a repo UUID, check for matching repository
+            const parent = !repo ? await thunkAPI.dispatch(fetchParentMetafile(input.metafile)).unwrap() : undefined;
             repo = (parent && isVersionedMetafile(parent)) ? repoSelectors.selectById(state, parent.repo) : repo;
             if (repo) return repo;
         }
@@ -29,21 +29,11 @@ export const fetchRepo = createAppAsyncThunk<Repository | undefined, ExactlyOne<
         const { dir, worktreeDir } = await getWorktreePaths(filepath);
         if (!dir) return undefined;
 
-        // check root metafile for an existing repository
-        const rootMetafile = await thunkAPI.dispatch(fetchMetafile({ path: dir })).unwrap();
-        if (isVersionedMetafile(rootMetafile)) {
-            const repo = repoSelectors.selectById(state, rootMetafile.repo);
-            if (repo) return repo;
-        }
-
-        // no existing repo in parent or root metafiles; check by path and build a new repo if needed
-        if (isFilebasedMetafile(rootMetafile)) {
-            const existingRepo = repoSelectors.selectByRoot(state, rootMetafile.path);
-            const root = worktreeDir ? worktreeDir : dir;
-            const repo = existingRepo ? existingRepo : await thunkAPI.dispatch(buildRepo(root)).unwrap();
-            return repo;
-        }
-        return undefined;
+        // check root for existing repository
+        const root = worktreeDir ? worktreeDir : dir;
+        const existingRepo = repoSelectors.selectByRoot(state, root);
+        const repo = existingRepo ? existingRepo : await thunkAPI.dispatch(buildRepo(root)).unwrap();
+        return repo;
     }
 )
 
@@ -52,7 +42,7 @@ export const buildRepo = createAppAsyncThunk<Repository, PathLike>(
     async (filepath, thunkAPI) => {
         const { dir } = await getWorktreePaths(filepath);
         const { url, oauth } = await getRemoteConfig(dir);
-        if (dir) await worktreePrune({ dir: dir, verbose: true }); // Prune worktree information to remove stale linked branches
+        if (dir) await worktreePrune({ dir: dir, verbose: true }); // prune worktree information to remove stale linked branches
         const current = dir ? await revParse({ dir: dir, options: ['abbrevRef'], args: 'HEAD' }) : undefined;
         const branches = dir ? await thunkAPI.dispatch(fetchBranches(dir)).unwrap() : { local: [], remote: [] };
         const { local, remote } = { local: branches.local.map(branch => branch.id), remote: branches.remote.map(branch => branch.id) };
@@ -61,13 +51,9 @@ export const buildRepo = createAppAsyncThunk<Repository, PathLike>(
             id: v4(),
             name: url ? extractRepoName(url.href) : (dir ? extractFilename(dir) : ''),
             root: dir ? dir : '',
-            /**
-             * TODO: The corsProxy is just a stubbed URL for now, but eventually we need to support Cross-Origin 
-             * Resource Sharing (CORS) since isomorphic-git requires it
-             */
             corsProxy: 'https://cors-anywhere.herokuapp.com',
             url: url ? url.href : '',
-            default: current[0] ? current[0].ref : '',
+            default: current ?? '',
             local: local,
             remote: remote,
             oauth: oauth ? oauth : 'github',
