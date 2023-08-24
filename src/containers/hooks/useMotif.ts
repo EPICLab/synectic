9f02cb9b80e334e677aacfa473345fec773c9357
Nicholas Nelson <nelsonni@oregonstate.edu>
2023-08-24 16:25:41 -0700

    colorPicker helper function for consistent status colors

--- src/containers/hooks/useMotif.ts
+++ src/containers/hooks/useMotif.ts
@@ -4,28 +4,52 @@ import { Metafile, isFileMetafile, isVersionedMetafile } from '../../store/slice
 import { getConflictingChunks, isDefined, isModified, isStaged, isUnmerged } from '../utils';
 
 type useMotifHook = {
-    color: string | undefined,
-    icon: SvgIconComponent | undefined
-}
+  color: string | undefined;
+  icon: SvgIconComponent | undefined;
+};
+
+const colorPicker = (colorName: string): string | undefined => {
+  switch (colorName) {
+    case 'red':
+      return '#da6473';
+    case 'blue':
+      return '#61aeee';
+    case 'orange':
+      return '#d19a66';
+    case 'green':
+      return '#50c878';
+  }
+  return undefined;
+};
 
 export const useFileMotif = (metafile: Metafile | undefined): useMotifHook => {
-    if (isDefined(metafile) && isFileMetafile(metafile) && isVersionedMetafile(metafile)) {
-        const conflicted = isUnmerged(metafile.status) || getConflictingChunks(metafile.content).length > 0;
-        const staged = isStaged(metafile.status);
-        const unstaged = isModified(metafile.status);
-        const icon = conflicted ? Warning : staged ? Remove : unstaged ? Add : undefined;
-        // #da6473 => red, #61aeee => blue, #d19a66 => orange
-        const color = conflicted ? '#da6473' : staged ? '#61aeee' : unstaged ? '#d19a66' : undefined;
-        return { color: color, icon: icon };
-    }
-    return { color: undefined, icon: undefined };
-}
+  if (isDefined(metafile) && isFileMetafile(metafile) && isVersionedMetafile(metafile)) {
+    const conflicted =
+      isUnmerged(metafile.status) || getConflictingChunks(metafile.content).length > 0;
+    const staged = isStaged(metafile.status);
+    const unstaged = isModified(metafile.status);
+    const icon = conflicted ? Warning : staged ? Remove : unstaged ? Add : undefined;
+    const color = conflicted
+      ? colorPicker('red')
+      : staged
+      ? colorPicker('blue')
+      : unstaged
+      ? colorPicker('orange')
+      : undefined;
+    return { color: color, icon: icon };
+  }
+  return { color: undefined, icon: undefined };
+};
 
 export const useBranchMotif = (branch: Branch | undefined): useMotifHook => {
-    if (isDefined(branch)) {
-        // #da6473 => red, #61aeee => blue
-        const color = branch.status === 'unmerged' ? '#da6473' : branch.linked ? '#61aeee' : undefined;
-        return { color: color, icon: undefined };
-    }
-    return { color: undefined, icon: undefined };
-}
+  if (isDefined(branch)) {
+    const color =
+      branch.status === 'unmerged'
+        ? colorPicker('red')
+        : branch.linked
+        ? colorPicker('blue')
+        : undefined;
+    return { color: color, icon: undefined };
+  }
+  return { color: undefined, icon: undefined };
+};
