0236bd3dc2fce64e3c7add82e30dd67d4a9c6d41
Nicholas Nelson <nelsonni@oregonstate.edu>
2023-09-09 16:58:06 -0700

    JSDoc line length alignment

--- src/containers/utils.ts
+++ src/containers/utils.ts
@@ -35,7 +35,8 @@ export type ExpandRecursively<T> = T extends object
 export type Override<T, U> = Omit<T, keyof U> & U;
 
 /**
- * Requires all properties to be nullable (i.e. `null` or `undefined` or `void`); the inverse of `NonNullable<T>`.
+ * Requires all properties to be nullable (i.e. `null` or `undefined` or `void`); the inverse of
+ * `NonNullable<T>`.
  * Inspired by: https://javascript.plainenglish.io/typescript-advanced-mapped-and-conditional-types-2d10c96042fe
  */
 export type Nullable<T> = {
@@ -88,13 +89,15 @@ export type WithRequired<T, K extends keyof T> = Omit<T, K> & Required<Pick<T, K
 export type WithOptional<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
 
 /**
- * From T, set the value to never and optional for all properties that are exclusive to T (i.e. are not assignable to U).
+ * From T, set the value to never and optional for all properties that are exclusive to T (i.e. are
+ * not assignable to U).
  * Reused from: https://github.com/ts-essentials/ts-essentials/blob/e0307a2e54bb6ae55666dfe434f840ccfc04e0c5/lib/types.ts
  */
 export type Without<T, U> = { [P in Exclude<keyof T, keyof U>]?: never };
 
 /**
- * Require type to exclusively match either T or U, but not both. This is a `mutually exclusive or` (XOR) operation between types.
+ * Require type to exclusively match either T or U, but not both. This is a `mutually exclusive or`
+ * (XOR) operation between types.
  * Reused from: https://github.com/ts-essentials/ts-essentials/blob/e0307a2e54bb6ae55666dfe434f840ccfc04e0c5/lib/types.ts
  * Inspired from: https://github.com/Microsoft/TypeScript/issues/14094
  */
@@ -142,7 +145,8 @@ export const isFilled = <T>(t: T | null): t is T => {
  * Generic for comparing possible updates against the current state of an object.
  * @param object The original state object.
  * @param properties The updated state properties.
- * @returns {boolean} A boolean indicating true if at least one property is new or contains modified values, false otherwise.
+ * @returns {boolean} A boolean indicating true if at least one property is new or contains modified
+ * values, false otherwise.
  */
 export const hasUpdates = <T extends Record<string | number | symbol, unknown>>(
   object: T,
@@ -169,7 +173,8 @@ export const isNumber = (maybeNumber: string | undefined): boolean => {
 /**
  * Check for a string containing a quote-encapsulated value (e.g. `a = '"quote"'`).
  * @param maybeQuote A string possibly containing a quote-encapsulated string value.
- * @returns {boolean} A boolean indicating true if the paraemter is quote-encapsulated, false otherwise.
+ * @returns {boolean} A boolean indicating true if the paraemter is quote-encapsulated, false
+ * otherwise.
  */
 export const isWrappedQuote = (maybeQuote: string): boolean => {
   return (
@@ -188,7 +193,8 @@ export const isStaged = (status: GitStatus): boolean =>
   ['added', 'modified', 'deleted'].includes(status);
 
 /**
- * Check for status indicating changes to a file that differ from the index, but have not been staged.
+ * Check for status indicating changes to a file that differ from the index, but have not been
+ * staged.
  * @param status An enumerated git status value.
  * @returns {boolean} A boolean indicating true if modified and unstaged, and false otherwise.
  */
@@ -216,8 +222,8 @@ export const isRenderer = (): 'renderer' | 'main' => {
 /**
  * Check for conflicting chunks in a string (i.e. code surrounded by `<<<<<<<` and `>>>>>>>`).
  * @param content A string containing code that possibly includes conflicting chunks.
- * @returns {number[]} An array of indices representing the starting point for each conflicting chunk
- * found in the string, or empty if clean.
+ * @returns {number[]} An array of indices representing the starting point for each conflicting
+ * chunk found in the string, or empty if clean.
  */
 export const getConflictingChunks = (content: string): number[] => {
   const conflictPattern = /<<<<<<<[^]+?=======[^]+?>>>>>>>/gm;
@@ -242,7 +248,8 @@ export const deserialize = <T>(json: string): T => JSON.parse(json) satisfies T;
  * @param predicate A predicate function that resolves to true if element `e` meets
  * the inclusion requirements, and false otherwise.
  * @returns {[object[], object[]]} The resulting array of arrays where elements that passed the
- * predicate are in the left subarray and elements that failed the predicate are in the right subarray.
+ * predicate are in the left subarray and elements that failed the predicate are in the right
+ * subarray.
  */
 export const partition = <T>(array: T[], predicate: (e: T) => boolean): [T[], T[]] => {
   return array.reduce(
@@ -255,15 +262,19 @@ export const partition = <T>(array: T[], predicate: (e: T) => boolean): [T[], T[
 };
 
 /**
- * Generic for partitioning two arrays into the symmetric differences, also known as the disjunctive union, and the
- * intersection given a predicate that indicates whether an element should be in the intersection subarray.
+ * Generic for partitioning two arrays into the symmetric differences, also known as the disjunctive
+ * union, and the intersection given a predicate that indicates whether an element should be in the
+ * intersection subarray.
  * @param array1 An array of elements.
  * @param array2 An array of elements.
- * @param predicate A predicate function that resolves to true if element `e1` from `array1` has a equivalent complement
+ * @param predicate A predicate function that resolves to true if element `e1` from `array1` has a
+ * equivalent complement
  * in `array2`, and false otherwise.
- * @returns {Array.<[object[], object[], object[]]>} The resulting array of arrays where symmetrically different elements from `array1`
- * are in the left subarray, intersection subarray containing pairs of element `e1` from `array1` and the complementary element
- * `e2` from `array2` in the center subarray, and the symmetrically different elements from `array2` are in the right subarray.
+ * @returns {Array.<[object[], object[], object[]]>} The resulting array of arrays where
+ * symmetrically different elements from `array1` are in the left subarray, intersection subarray
+ * containing pairs of element `e1` from `array1` and the complementary element `e2` from `array2`
+ * in the center subarray, and the symmetrically different elements from `array2` are in the right
+ * subarray.
  */
 export const symmetrical = <T, U>(
   array1: T[],
@@ -339,7 +350,8 @@ export const removeDuplicates = <T>(arr: T[], comparator: (a: T, b: T) => boolea
  * @param arr An array of elements.
  * @param predicate A predicate function that resolves to true if element `e` meets
  * the filter inclusion requirements, and false otherwise.
- * @returns {Promise<object[]>} The resulting array devoid of elements that did not meet the predicate.
+ * @returns {Promise<object[]>} The resulting array devoid of elements that did not meet the
+ *  predicate.
  */
 export const asyncFilter = async <T>(
   arr: T[],
@@ -350,26 +362,13 @@ export const asyncFilter = async <T>(
 };
 
 /**
- * Generic for flattening and filtering an object given a set of filtering keys for inclusion in the resulting object.
- * @param obj An object containing key-value indexed fields.
- * @param filter An array of key strings that indicate which key-value pairs should be included or excluded.
- * @returns {object} The resulting object devoid of key-value fields that did not match the key filter, or an empty object.
- */
-export const filterObject = <V, T extends Record<string, V>>(
-  obj: T,
-  filter: string[]
-): ReturnType<typeof flattenObject<T>> => {
-  return Object.entries(flattenObject(obj))
-    .filter(([key]) => filter.includes(key))
-    .reduce((accumulator, [k, v]) => ({ ...accumulator, [k]: v }), {});
-};
-
-/**
- * Expand an array of path elements (i.e. a dot-format path such as `user.name`) into a successive series of named
- * objects that contain any subsequent path elements and/or the updated value.
- * @param path An array of path elements ordered in sequence (i.e. `user.name.lastName` would be `[user, name, lastName]`).
+ * Expand an array of path elements (i.e. a dot-format path such as `user.name`) into a successive
+ * series of named objects that contain any subsequent path elements and/or the updated value.
+ * @param path An array of path elements ordered in sequence (i.e. `user.name.lastName` would be
+ * `[user, name, lastName]`).
  * @param value The value to insert or update at the specific path in the return object.
- * @returns {object} An object containing a key-value structure of embedded container objects according to the given path.
+ * @returns {object} An object containing a key-value structure of embedded container objects
+ * according to the given path.
  */
 export const objectifyPath = (
   path: Array<string | number>,
@@ -382,9 +381,9 @@ export const objectifyPath = (
 };
 
 /**
- * Compares two `Map` objects for equality; only supports `Map` and not objects with key-value pairs (i.e.
- * `const map: {[key: string]: value}`). Checks for same map sizes, then tests all underlying key-value entries for
- * equality between maps.
+ * Compares two `Map` objects for equality; only supports `Map` and not objects with key-value pairs
+ * (i.e. `const map: {[key: string]: value}`). Checks for same map sizes, then tests all underlying
+ * key-value entries for equality between maps.
  * @param map1 A `Map` object.
  * @param map2 A `Map` object.
  * @returns {boolean} A boolean indicating true if the `Map` objects are equal, or false otherwise.
@@ -403,8 +402,8 @@ export const equalMaps = <K, V>(map1: Map<K, V>, map2: Map<K, V>): boolean => {
 };
 
 /**
- * Compares two `Array` objects for equality. Checks for same array length, then tests all underlying elements
- * for equality between arrays.
+ * Compares two `Array` objects for equality. Checks for same array length, then tests all
+ * underlying elements for equality between arrays.
  * @param arr1 An `Array` object.
  * @param arr2 An `Array` object.
  * @returns {boolean} A boolean indicating true if the `Array` objects are equal, or false otherwise.
@@ -418,12 +417,13 @@ export const equalArrays = <T>(arr1: Array<T>, arr2: Array<T>): boolean => {
 };
 
 /**
- * Compares two objects for equality based on a filtered subset of properties. Checks for equality on the provided
- * properties only, enabling masked equality checks between complex objects.
+ * Compares two objects for equality based on a filtered subset of properties. Checks for equality
+ * on the provided properties only, enabling masked equality checks between complex objects.
  * @param obj1 A JavaScript object.
  * @param obj2 A JavaScript object.
  * @param props An array of keys derived from the type definition of comparable objects.
- * @returns {boolean} A boolean indicating true if the objects are equal among all selected properties, or false otherwise.
+ * @returns {boolean} A boolean indicating true if the objects are equal among all selected
+ * properties, or false otherwise.
  */
 export const filteredObjectEquality = <T, K extends keyof T>(
   obj1: T,
@@ -434,12 +434,14 @@ export const filteredObjectEquality = <T, K extends keyof T>(
 };
 
 /**
- * Compares two arrays of object for equality based on a filtered subset of object properties. Checks for equality on the
- * provided properties only, enabling masked equality checks between arrays of complex objects.
+ * Compares two arrays of object for equality based on a filtered subset of object properties.
+ * Checks for equality on the provided properties only, enabling masked equality checks between
+ * arrays of complex objects.
  * @param arr1 An `Array` object containing JavaScript objects.
  * @param arr2 An `Array` object containing JavaScript objects.
  * @param props An array of keys derived from the type definition of comparable objects.
- * @returns {boolean} A boolean indicating true if all objects in both arrays are equal among all selected properties, or false otherwise.
+ * @returns {boolean} A boolean indicating true if all objects in both arrays are equal among all
+ * selected properties, or false otherwise.
  */
 export const filteredArrayEquality = <T, K extends keyof T>(
   arr1: T[],
@@ -455,14 +457,16 @@ export const filteredArrayEquality = <T, K extends keyof T>(
 };
 
 /**
- * Compares two `ArrayBufferLike` objects for equality; compatible objects include `TypedArray`, `DataView`,
- * and `Buffer`. However, `Buffer` has `.equals()` and `.compare()` functions for comparisons and these should
- * be used in lieu of this function. `TypedArray` is a class of objects, which includes all of the following:
- * `Int8Array`, `Uint8Array`, `Uint8ClampedArray`, `Int16Array` ,`Int32Array`, `Uint32Array`, `Float32Array`,
- * `Float64Array`, `BigInt64Array`, `BigUint64Array`.
+ * Compares two `ArrayBufferLike` objects for equality; compatible objects include `TypedArray`,
+ * `DataView`, and `Buffer`. However, `Buffer` has `.equals()` and `.compare()` functions for
+ * comparisons and these should be used in lieu of this function. `TypedArray` is a class of
+ * objects, which includes all of the following: `Int8Array`, `Uint8Array`, `Uint8ClampedArray`,
+ * `Int16Array` ,`Int32Array`, `Uint32Array`, `Float32Array`, `Float64Array`, `BigInt64Array`,
+ * `BigUint64Array`.
  * @param buf1 An `ArrayBufferLike` object.
  * @param buf2 An `ArrayBufferLike` object.
- * @returns {boolean} A boolean indicating true if the `ArrayBufferLike` objects are equal, or false otherwise.
+ * @returns {boolean} A boolean indicating true if the `ArrayBufferLike` objects are equal, or false
+ * otherwise.
  */
 export const equalArrayBuffers = (buf1: ArrayBufferLike, buf2: ArrayBufferLike): boolean => {
   if (buf1.byteLength != buf2.byteLength) return false;
@@ -477,7 +481,8 @@ export const equalArrayBuffers = (buf1: ArrayBufferLike, buf2: ArrayBufferLike):
 /**
  * Convert a `Buffer` object into an `ArrayBuffer` object.
  * @param buf A `Buffer` object representing binary data in the form of a sequence of bytes.
- * @returns {ArrayBuffer} A new `ArrayBuffer` object containing a copy of all data in `Buffer` object.
+ * @returns {ArrayBuffer} A new `ArrayBuffer` object containing a copy of all data in `Buffer`
+ * object.
  */
 export const toArrayBuffer = (buf: Buffer): ArrayBuffer => {
   const ab = new ArrayBuffer(buf.length);
@@ -500,9 +505,10 @@ export const getRandomInt = (min: number, max: number): number => {
 };
 
 /**
- * Scheduled execution of a one-time no-op callback after a delay of `ms` milliseconds. Since this relies on
- * [`setTimeout()`](https://nodejs.org/api/timers.html#settimeoutcallback-delay-args) from Node.js, we do not guarantee the exact timing of when the callback will
- * fire, nor of their ordering. The callback will be called as close as possible to the time specified.
+ * Scheduled execution of a one-time no-op callback after a delay of `ms` milliseconds. Since this
+ * relies on [`setTimeout()`](https://nodejs.org/api/timers.html#settimeoutcallback-delay-args) from
+ * Node.js, we do not guarantee the exact timing of when the callback will fire, nor of their
+ * ordering. The callback will be called as close as possible to the time specified.
  * @param ms The number of milliseconds to wait before callback returns.
  * @returns {Promise<unknown>} A Promise object containing a no-op callback.
  */
